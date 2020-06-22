bbb29daf31765c0bebd5407c9db6531c6a6903ba
nino <ninolin@ioneit.com>
2020-06-23 00:37:52 +0800

    dashboard & change AppMain backgroud color

---
+++ src/views/dashboard/components/priority_pie.vue
@@ -0,0 +1,74 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+
+<script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    }
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+      this.chart.setOption({
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b} : {c} ({d}%)'
+        },
+        color: ['#f56c6c', '#e6a23c'],
+        legend: {
+          left: 'center',
+          bottom: '10',
+          data: ['Urgent', 'High']
+        },
+        series: [
+          {
+            name: 'ASSIGN ISSUE PRIORITY',
+            type: 'pie',
+            roseType: 'radius',
+            radius: [15, 95],
+            center: ['50%', '38%'],
+            data: [
+              { value: 32, name: 'Urgent' },
+              { value: 24, name: 'High' }
+            ],
+            animationEasing: 'cubicInOut',
+            animationDuration: 2600
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
