81f542958dc74a07f167e91456510427d313d850
Nathan <chu0531channel@gmail.com>
2020-09-17 23:40:19 +0800

    roadmap

--- src/api/projects.js
+++ src/api/projects.js
@@ -53,18 +53,20 @@ export function getProjectAssignable(pId) {
 }
 
 //取得專案議題狀態 [ 0_1 單一專案概述]
-export function getProjectIssueProgress(pId) {
+export function getProjectIssueProgress(pId, params = {}) {
   return request({
     url: `/project/${pId}/issues_progress`,
-    method: 'GET'
+    method: 'GET',
+    params
   })
 }
 
 //取得專案議題篩選條件狀態 [0.1_單一專案概述]
-export function getProjectIssueStatistics(pId) {
+export function getProjectIssueStatistics(pId, params) {
   return request({
     url: `/project/${pId}/issues_statistics`,
-    method: 'GET'
+    method: 'GET',
+    params
   })
 }
 
@@ -125,5 +127,14 @@ export function deleteProjectVersion(pId, vId) {
   return request({
     url: `/project/${pId}/version/${vId}`,
     method: 'DELETE'
+  })
+}
+
+// 取得project內所有issue清單 [0.5 RoadMap_Detail]
+export function getProjectIssueListByVersion(pId, params) {
+  return request({
+    url: `/project/${pId}/issues`,
+    method: 'GET',
+    params
   })
 }
--- src/views/ProjectRoadmap/index.vue
+++ src/views/ProjectRoadmap/index.vue
@@ -1,47 +1,63 @@
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import ProjectListSelector from '../../components/ProjectListSelector'
 import projectBar from './components/project_bar'
+import {
+  getProjectVersion,
+  getProjectIssueProgress,
+  getProjectIssueListByVersion,
+  getProjectIssueStatistics
+} from '@/api/projects'
 
 export default {
   name: 'Dashboard',
   components: {
-    projectBar
+    projectBar,
+    ProjectListSelector
   },
   data() {
     return {
-      dataList: [
-        {
-          value: 100,
-          type: 'success',
-          version: '1.0.0'
-        },
-        {
-          value: 77,
-          type: null,
-          version: '1.0.1'
-        },
-        {
-          value: 0,
-          type: null,
-          version: '2.0.0'
-        }
-      ],
-      workList: [
-        { value: 'User Login', type: 'User Case', number: '2' },
-        { value: 'Login Page', type: 'Mission', number: '3' },
-        { value: 'Reset Password Error', type: 'Bug', number: '5' },
-        { value: 'Adding Work', type: 'User Case', number: '3' }
-      ]
+      listLoading: true,
+      contentLoading: false,
+      workLoad: '',
+      workLoadData: {},
+      workLoadTypes: [],
+      workLoadSelected: {},
+      activeNames: '',
+      dataList: [],
+      workList: {},
+      activedIndex: []
     }
   },
   computed: {
-    activeNames() {
-      return this.dataList.map((e, idx) => {
-        return idx
-      })
+    ...mapGetters(['userProjectList', 'projectSelectedId'])
+  },
+  watch: {
+    projectSelectedId(projectId) {
+      this.fetchAll()
     }
   },
+  async created() {
+    this.fetchAll()
+  },
   methods: {
+    async fetchAll() {
+      this.listLoading = true
+      const res = await getProjectVersion(this.projectSelectedId)
+      const resAll = await Promise.all(
+        res.data.versions.map(({ id }) => getProjectIssueProgress(this.projectSelectedId, { fixed_version_id: id }))
+      )
+      this.dataList = resAll.map(({ data }, idx) => {
+        return { ...data, vId: res.data.versions[idx].id, name: res.data.versions[idx].name }
+      })
+      this.listLoading = false
+    },
+    returnVersionType(item) {
+      const percentage = this.percentageMethod(item)
+      if (percentage === 0) return 'danger'
+      if (percentage === 100) return 'success'
+      return 'primary'
+    },
     returnTagType(type) {
       switch (type) {
         case 'User Case':
@@ -53,42 +69,80 @@ export default {
         default:
           return null
       }
+    },
+    percentageMethod(item) {
+      if (item.total_issue > 0 && item.unfinish_number === 0) return 100
+      return item.total_issue > 0 ? ((item.total_issue - item.unfinish_number) / item.total_issue) * 100 : 0
+    },
+    onCollapseChange(value) {
+      if (!value) return
+      if (this.activedIndex.includes(value)) return
+      this.activedIndex.push(value)
+      this.fetchDetails(value)
+    },
+    async fetchDetails(vId) {
+      this.contentLoading = true
+      const data = await getProjectIssueListByVersion(this.projectSelectedId, { fixed_version_id: vId })
+      this.$set(this.workList, vId, data.data)
+
+      const statistics = await getProjectIssueStatistics(this.projectSelectedId, { fixed_version_id: vId })
+      this.workLoadData = statistics.data
+      this.workLoadTypes = Object.keys(this.workLoadData).map(key => {
+        return { id: key, name: key }
+      })
+      this.workLoad = this.workLoadTypes[0].id
+      this.workLoadSelected = this.workLoadData[this.workLoad]
+      this.contentLoading = false
+    },
+    onWorkLoadChange(value) {
+      this.workLoadSelected = this.workLoadData[value]
     }
   }
 }
 </script>
 
 <template>
-  <div class="dashboard-container">
+  <div class="dashboard-container" v-loading="listLoading">
+    <div>
+      <project-list-selector />
+    </div>
+    <el-divider />
     <el-row :gutter="12">
       <el-col :span="24">
-        <el-card shadow="hover">
-          <el-collapse v-model="activeNames">
-            <el-collapse-item v-for="(item, idx) in dataList" :name="idx" :key="item.version">
+        <el-card shadow="hover" body-style="padding-top:0px;padding-bottom:0px">
+          <el-collapse v-model="activeNames" @change="onCollapseChange" :accordion="true">
+            <el-collapse-item v-for="item in dataList" :name="item.vId" :key="item.vId">
               <template slot="title">
                 <div class="titleProgress">
-                  Version&nbsp;&nbsp;<el-tag effect="dark" :type="item.type">{{ item.version }}</el-tag>
-                  <el-progress :percentage="item.value" :status="item.type"></el-progress>
+                  <el-tag effect="dark" :type="returnVersionType(item)">{{ item.name }}</el-tag>
+                  <el-progress :percentage="percentageMethod(item)" :status="item.type"></el-progress>
                 </div>
               </template>
               <el-divider />
-              <div class="contentBody">
+              <div class="contentBody" v-loading="contentLoading">
                 <el-row :gutter="12">
                   <el-col :span="8">
-                    <el-table :data="workList" style="width: 100%" border stripe>
+                    <el-table :data="workList[item.vId]" style="width: 100%" border stripe>
                       <el-table-column prop="lsit" label="Work List">
                         <template slot-scope="scope">
-                          <el-tag :type="returnTagType(scope.row.type)">{{ scope.row.type }}</el-tag>
+                          <el-tag :type="scope.row.issue_priority === '急' ? 'warning' : 'danger'">{{
+                            scope.row.issue_priority
+                          }}</el-tag>
                           <span style="color: #409EFF">
-                            {{ scope.row.value }}
-                            <b>#{{ scope.row.number }}</b>
+                            [{{ scope.row.issue_category }}]
+                            <b>#{{ scope.row.issue_name }}</b>
                           </span>
                         </template>
                       </el-table-column>
                     </el-table>
                   </el-col>
                   <el-col :span="16">
-                    <project-bar />
+                    <el-select v-model="workLoad" placeholder="select a project" @change="onWorkLoadChange">
+                      <el-option v-for="items in workLoadTypes" :key="items.id" :label="items.name" :value="items.id">
+                      </el-option>
+                    </el-select>
+                    <el-divider />
+                    <project-bar v-if="workList[item.vId]" :the-data="workLoadSelected" />
                   </el-col>
                 </el-row>
               </div>
@@ -115,9 +169,9 @@ export default {
   }
 }
 >>> .el-collapse-item__header {
-  height: 78px;
+  height: 84px;
 }
 .titleProgress {
-  width: 30%;
+  width: 100%;
 }
 </style>
