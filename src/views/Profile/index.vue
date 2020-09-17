d628be098dad825efd978e468130b5d87c2158ed
nino <a7872278722@gmail.com>
2020-09-17 23:58:12 +0800

    fix: fix update user bug

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -5,28 +5,27 @@
         <el-tab-pane label="Basic">
           <div style="padding: 0px 25px;">
             <h3>Profile Basic Setting</h3>
-            <el-form 
-              :model="userProfileForm" 
-              :rules="userProfileRules" 
-              ref="userProfileForm" 
-              label-width="100px" 
+            <el-form
+              ref="userProfileForm"
+              :model="userProfileForm"
+              :rules="userProfileRules"
+              label-width="100px"
               class="demo-ruleForm"
               :label-position="labelPosition"
             >
               <el-form-item label="Name" prop="userName">
-                <el-input v-model="userProfileForm.userName" style="width: 250px;"></el-input>
+                <el-input v-model="userProfileForm.userName" style="width: 250px;" />
               </el-form-item>
               <el-form-item label="Email" prop="userEmail">
-                <el-input v-model="userProfileForm.userEmail" style="width: 250px;"></el-input>
+                <el-input v-model="userProfileForm.userEmail" style="width: 250px;" />
               </el-form-item>
               <el-form-item label="Phone" prop="userPhone">
-                <el-input v-model="userProfileForm.userPhone" style="width: 250px;"></el-input>
+                <el-input v-model="userProfileForm.userPhone" style="width: 250px;" />
               </el-form-item>
             </el-form>
 
             <el-row style="margin-top: 15px;">
               <el-col :span="8">
-                <!-- <el-button type="primary" @click="handleUpdateUserProfile">Save</el-button> -->
                 <el-button type="primary" @click="submitUpdateUserProfile('userProfileForm')">Save</el-button>
               </el-col>
             </el-row>
@@ -35,39 +34,24 @@
         <el-tab-pane label="Security">
           <div style="padding: 0px 25px;">
             <h3>Profile Security Setting</h3>
-            <el-row>
-              <el-col :span="8" style="line-height: 30px;">
-                Old Password
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="8">
-                <el-input v-model="userOldPwd" type="password"/>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="8" style="line-height: 30px;">
-                New Password
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="8">
-                <el-input v-model="userNewPwd" type="password"/>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="8" style="line-height: 30px;">
-                Repeat New Password
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="8">
-                <el-input v-model="userRepeatNewPwd" type="password"/>
-              </el-col>
-            </el-row>
+            <el-form
+              ref="userPwdForm"
+              :model="userPwdForm"
+              :rules="userPwdFormRules"
+              label-width="100px"
+              class="demo-ruleForm"
+              :label-position="labelPosition"
+            >
+              <el-form-item label="New Password" prop="userNewPwd">
+                <el-input v-model="userPwdForm.userNewPwd" type="password" style="width: 250px;" />
+              </el-form-item>
+              <el-form-item label="Repeat New Password" prop="userRepeatNewPwd">
+                <el-input v-model="userPwdForm.userRepeatNewPwd" type="password" style="width: 250px;" />
+              </el-form-item>
+            </el-form>
             <el-row style="margin-top: 15px;">
               <el-col :span="8">
-                <el-button type="primary" @click="handleUpdateUserPwd">Save</el-button>
+                <el-button type="primary" @click="handleUpdateUserPwd('userPwdForm')">Save</el-button>
               </el-col>
             </el-row>
           </div>
@@ -84,6 +68,13 @@ import { Message } from 'element-ui'
 
 export default {
   data() {
+    const checkRepeatPwd = (rule, value, callback) => {
+      if (value !== this.userPwdForm.userNewPwd) {
+        callback(new Error('password not same'))
+      } else {
+        callback()
+      }
+    }
     return {
       labelPosition: 'top',
       tabPosition: 'left',
@@ -100,12 +91,21 @@ export default {
         userPhone: 0
       },
       userProfileRules: {
-        userName: [
-          { required: true, message: 'Please input user name', trigger: 'blur' }
-        ],
+        userName: [{ required: true, message: 'Please input user name', trigger: 'blur' }],
         userEmail: [
           { required: true, message: 'Please input email', trigger: 'blur' },
           { type: 'email', message: 'Invalid email', trigger: ['blur', 'change'] }
+        ]
+      },
+      userPwdForm: {
+        userNewPwd: '',
+        userRepeatNewPwd: ''
+      },
+      userPwdFormRules: {
+        userNewPwd: [{ required: true, message: 'Please input new password', trigger: 'blur' }],
+        userRepeatNewPwd: [
+          { required: true, message: 'Please input repeat password', trigger: 'blur' },
+          { validator: checkRepeatPwd, trigger: 'blur' }
         ]
       }
     }
@@ -113,45 +113,48 @@ export default {
   async created() {
     this.userId = store.getters.userId
     const userProfile = await getInfo(this.userId)
-    this.userProfileForm.userName = userProfile.data.usernmae
+    this.userProfileForm.userName = userProfile.data.name
     this.userProfileForm.userEmail = userProfile.data.email
     this.userProfileForm.userPhone = userProfile.data.phone
   },
   methods: {
-    submitUpdateUserProfile(formName) {
-      this.$refs[formName].validate(async (valid) => {
-          if (valid) {
-            const data = {
-              'name': this.userProfileForm.userName,
-              'email': this.userProfileForm.userEmail,
-              'phone': this.userProfileForm.userPhone
-            }
-            await updateUser(this.userId, data)
-            Message({
-              message: 'update successful',
-              type: 'success',
-              duration: 1 * 1000
-            })
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-    },
-    async handleUpdateUserPwd() {
-      console.log('aaa')
-      if (this.userNewPwd !== this.userRepeatNewPwd) {
-        Message({
-          message: 'new password not equal repeat new password',
-          type: 'error',
-          duration: 1 * 1000
-        })
+    checkRepeatPwd(rule, value, callback) {
+      if (value !== this.userForm.password) {
+        callback(new Error('password not same'))
+      } else {
+        callback()
       }
-      await updateUser(this.userId, { 'password': this.userNewPwd })
-      Message({
-        message: 'update successful',
-        type: 'success',
-        duration: 1 * 1000
+    },
+    submitUpdateUserProfile(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          const data = {
+            name: this.userProfileForm.userName,
+            email: this.userProfileForm.userEmail,
+            phone: this.userProfileForm.userPhone
+          }
+          await updateUser(this.userId, data)
+          Message({
+            message: 'update successful',
+            type: 'success',
+            duration: 1 * 1000
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    async handleUpdateUserPwd(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          await updateUser(this.userId, { password: this.userPwdForm.userNewPwd })
+          Message({
+            message: 'update successful',
+            type: 'success',
+            duration: 1 * 1000
+          })
+        }
       })
     }
   }
