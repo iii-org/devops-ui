c64e300bb8d65d1c1a410d393c681adeb6ec0d83
Romulus Urakagi Tsai <urakagi@gmail.com>
2020-10-26 15:22:03 +0800

    Rewrite graph generating codes

--- src/views/ProjectGraph/index.vue
+++ src/views/ProjectGraph/index.vue
@@ -32,7 +32,7 @@ export default {
       this.listLoading = true
       try {
         const res = await getGitGraphByRepo(this.selectedRepoId)
-        this.createGraph(res.data.branch_commit_list)
+        this.createGraph(res.data)
       } catch (err) {
         console.error(err)
       } finally {
@@ -40,23 +40,127 @@ export default {
       }
     },
     createGraph(data) {
-      console.log(data)
       // Get the graph container HTML element.
       const graphContainer = document.getElementById('graph-container')
 
       // Instantiate the graph.
       const gitgraph = createGitgraph(graphContainer)
 
-      data.forEach(branch => {
-        const _branch = gitgraph.branch(branch.branch)
-        branch.commit_list.forEach(commit => {
-          _branch.commit({
-            subject: commit.message,
-            author: commit.author_name + ' <' + commit.author_name + '>'
-          })
-        })
-      })
+      const branches = {}
+      const commits = {}
+      const commit_order = []
 
+      function Commit(json) {
+        this.children = []
+        for (const key in json) {
+          this[key] = json[key]
+        }
+        delete this.branch_name
+        this.branches = [json.branch_name]
+        this.addBranch = function(branch_name) {
+          this.branches.push(branch_name)
+        }
+        this.mainBranch = function() {
+          if (this.branches.includes('master')) {
+            return 'master'
+          }
+          if (this.branches.includes('main')) {
+            return 'main'
+          }
+          return this.branches[0]
+        }
+      }
+
+      function Branch(name) {
+        this.name = name
+        this.head = null
+        this.graph = undefined
+
+        this.addCommit = function(commit) {
+          if (this.head == null) {
+            this.graph = gitgraph.branch(this.name)
+          }
+          if (commit.parent_ids.length <= 1) {
+            // Regular commit
+            if (this.head == null || this.head.id === commit.parent_ids[0]) {
+              // Check if new branch begins
+              const commit_opts = {
+                author: commit.author_name,
+                hash: commit.id,
+                subject: commit.message
+              }
+              this.graph.commit(commit_opts)
+              this.head = commit
+            } else {
+              for (let i = 1; i < 9; i++) {
+                const newName = `${this.name}_${i}`
+                if (!(newName in branches)) {
+                  const newBranch = new Branch(newName)
+                  branches[newName] = newBranch
+                  newBranch.addCommit(commit)
+                  break
+                }
+              }
+            }
+          } else if (commit.parent_ids.length === 2) {
+            // Merge
+            for (const k in branches) {
+              if (branches[k].head != null) console.log(k, branches[k].head.id)
+            }
+            for (const k in branches) {
+              if (k === this.name) continue
+              const ob = branches[k]
+              if (ob.head.id === commit.parent_ids[0] ||
+                ob.head.id === commit.parent_ids[1]) {
+                const merge_opts = {
+                  branch: ob.name,
+                  commitOptions: {
+                    author: commit.author_name,
+                    hash: commit.id,
+                    subject: commit.message
+                  }
+                }
+                this.graph.merge(merge_opts)
+                this.head = commit
+                break
+              }
+            }
+          }
+        }
+      }
+      // Merge same commits
+      data.forEach(commit_json => {
+        const id = commit_json.id
+        if (id in commits) {
+          commits[id].addBranch(commit_json.branch_name)
+          return
+        }
+        commits[id] = new Commit(commit_json)
+        commit_order.push(id)
+      })
+      // Trace commit log
+      commit_order.forEach(id => {
+        const commit = commits[id]
+        const branch_name = commit.mainBranch()
+        let branch
+        if (branch_name in branches) {
+          branch = branches[branch_name]
+        } else {
+          branch = new Branch(branch_name)
+          branches[branch_name] = branch
+        }
+        branch.addCommit(commit)
+      })
+      // data.forEach(branch => {
+      //   const _branch = gitgraph.branch(branch.branch)
+      //   branch.commit_list.forEach(commit => {
+      //     _branch.commit({
+      //       subject: commit.message,
+      //       author: commit.author_name + ' <' + commit.author_name + '>'
+      //     })
+      //   })
+      // })
+      //
       // Simulate git commands with Gitgraph API.
       // const master = gitgraph.branch('master')
       // master.commit('Initial commit')
