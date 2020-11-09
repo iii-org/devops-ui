901ca629625a97fcb7a8c5a14b5c18a3a7d62e6d
nino <a7872278722@gmail.com>
2020-11-09 21:04:54 +0800

    feat: add issue can upload file

--- src/views/ProjectIssues/components/AddIssue.vue
+++ src/views/ProjectIssues/components/AddIssue.vue
@@ -7,17 +7,12 @@
     :close-on-click-modal="false"
     @close="handleClose"
   >
-    <el-form
-      ref="issueForm"
-      :model="issueForm"
-      :rules="issueFormRules"
-      class="custom-list"
-    >
+    <el-form ref="issueForm" :model="issueForm" :rules="issueFormRules" class="custom-list">
       <el-row>
-        <el-row v-if="parentid!=0">
+        <el-row v-if="parentid != 0">
           <el-col :span="12">
             <el-form-item>
-              <div style="font-weight:bold;">Parent issue : {{ parentname }}</div>
+              <div style="font-weight: bold">Parent issue : {{ parentname }}</div>
             </el-form-item>
           </el-col>
         </el-row>
@@ -28,13 +23,8 @@
         </el-col>
         <el-col :span="12">
           <el-form-item label="Assignee" prop="assigned_to_id">
-            <el-select v-model="issueForm.assigned_to_id" style="width:100%" filterable>
-              <el-option
-                v-for="item in issueAssigneeList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
+            <el-select v-model="issueForm.assigned_to_id" style="width: 100%" filterable>
+              <el-option v-for="item in issueAssigneeList" :key="item.value" :label="item.label" :value="item.value" />
             </el-select>
           </el-form-item>
         </el-col>
@@ -42,25 +32,15 @@
       <el-row>
         <el-col :span="12">
           <el-form-item label="Version" prop="fixed_version_id">
-            <el-select v-model="issueForm.fixed_version_id" style="width:100%" filterable>
-              <el-option
-                v-for="item in issueVersionList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
+            <el-select v-model="issueForm.fixed_version_id" style="width: 100%" filterable>
+              <el-option v-for="item in issueVersionList" :key="item.value" :label="item.label" :value="item.value" />
             </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="12">
           <el-form-item label="Type" prop="tracker_id">
-            <el-select v-model="issueForm.tracker_id" style="width:100%">
-              <el-option
-                v-for="item in issueTypeList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
+            <el-select v-model="issueForm.tracker_id" style="width: 100%">
+              <el-option v-for="item in issueTypeList" :key="item.value" :label="item.label" :value="item.value" />
             </el-select>
           </el-form-item>
         </el-col>
@@ -68,25 +48,15 @@
       <el-row>
         <el-col :span="12">
           <el-form-item label="Status" prop="status_id">
-            <el-select v-model="issueForm.status_id" style="width:100%">
-              <el-option
-                v-for="item in issueStatusList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
+            <el-select v-model="issueForm.status_id" style="width: 100%">
+              <el-option v-for="item in issueStatusList" :key="item.value" :label="item.label" :value="item.value" />
             </el-select>
           </el-form-item>
         </el-col>
         <el-col :span="12">
           <el-form-item label="Priority" prop="priority_id">
-            <el-select v-model="issueForm.priority_id" style="width:100%">
-              <el-option
-                v-for="item in issuePriorityList"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
+            <el-select v-model="issueForm.priority_id" style="width: 100%">
+              <el-option v-for="item in issuePriorityList" :key="item.value" :label="item.label" :value="item.value" />
             </el-select>
           </el-form-item>
         </el-col>
@@ -94,7 +64,7 @@
       <el-row>
         <el-col :span="12">
           <el-form-item label="Estimate" prop="estimated_hours">
-            <el-input-number v-model="issueForm.estimated_hours" label="please input hours" style="width:100%" />
+            <el-input-number v-model="issueForm.estimated_hours" label="please input hours" style="width: 100%" />
           </el-form-item>
         </el-col>
         <el-col :span="12">
@@ -104,7 +74,7 @@
               label="please input numbers"
               :min="0"
               :max="100"
-              style="width:100%"
+              style="width: 100%"
             />
           </el-form-item>
         </el-col>
@@ -116,7 +86,7 @@
               v-model="issueForm.start_date"
               type="date"
               placeholder="Select Date"
-              style="width: 100%;"
+              style="width: 100%"
               value-format="yyyy-MM-dd"
             />
           </el-form-item>
@@ -127,14 +97,30 @@
               v-model="issueForm.due_date"
               type="date"
               placeholder="Select Date"
-              style="width: 100%;"
+              style="width: 100%"
               value-format="yyyy-MM-dd"
             />
           </el-form-item>
         </el-col>
       </el-row>
       <el-row>
-        <el-col :span="24">
+        <el-col :span="12">
+          <el-form-item label="Upload" prop="upload">
+            <el-upload
+              ref="upload"
+              class="upload-file"
+              drag
+              action=""
+              :auto-upload="false"
+              :limit="1"
+              :on-exceed="handleExceed"
+              :on-change="handleChange"
+            >
+              <div class="el-upload__text">drap file here or <em>click upload</em></div>
+            </el-upload>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
           <el-form-item label="Desc." prop="description">
             <el-input v-model="issueForm.description" type="textarea" placeholder="please input description" />
           </el-form-item>
@@ -150,6 +136,7 @@
 <script>
 import { getIssueStatus, getIssueTracker, getIssuePriority } from '@/api/issue'
 import { getProjectUserList, getProjectVersion } from '@/api/projects'
+import { fileextension } from '../../../utils/extension.js'
 export default {
   props: {
     dialogVisible: {
@@ -192,41 +179,21 @@ export default {
         parent_id: ''
       },
       issueFormRules: {
-        subject: [
-          { required: true, message: 'Please input name', trigger: 'blur' }
-        ],
-        tracker_id: [
-          { required: true, message: 'Please select type', trigger: 'blur' }
-        ],
-        status_id: [
-          { required: true, message: 'Please select status', trigger: 'blur' }
-        ],
-        assigned_to_id: [
-          { required: true, message: 'Please select assigned', trigger: 'blur' }
-        ],
-        start_date: [
-          { required: true, message: 'Please select start date', trigger: 'blur' }
-        ],
-        due_date: [
-          { required: true, message: 'Please select due date', trigger: 'blur' }
-        ],
-        done_ratio: [
-          { required: true, message: 'Please input done ratio', trigger: 'blur' }
-        ],
-        estimated_hours: [
-          { required: true, message: 'Please input estimated hours', trigger: 'blur' }
-        ],
-        priority_id: [
-          { required: true, message: 'Please select priority', trigger: 'blur' }
-        ],
-        fixed_version_id: [
-          { required: false }
-        ],
-        description: [
-          { required: false }
-        ]
+        subject: [{ required: true, message: 'Please input name', trigger: 'blur' }],
+        tracker_id: [{ required: true, message: 'Please select type', trigger: 'blur' }],
+        status_id: [{ required: true, message: 'Please select status', trigger: 'blur' }],
+        assigned_to_id: [{ required: true, message: 'Please select assigned', trigger: 'blur' }],
+        start_date: [{ required: true, message: 'Please select start date', trigger: 'blur' }],
+        due_date: [{ required: true, message: 'Please select due date', trigger: 'blur' }],
+        done_ratio: [{ required: true, message: 'Please input done ratio', trigger: 'blur' }],
+        estimated_hours: [{ required: true, message: 'Please input estimated hours', trigger: 'blur' }],
+        priority_id: [{ required: true, message: 'Please select priority', trigger: 'blur' }],
+        fixed_version_id: [{ required: false }],
+        description: [{ required: false }]
       },
-      LoadingConfirm: false
+      LoadingConfirm: false,
+      uploadFileList: [],
+      extension: {}
     }
   },
   watch: {
@@ -244,6 +211,7 @@ export default {
   },
   mounted() {
     this.fetchData()
+    this.extension = fileextension()
   },
   methods: {
     fetchData() {
@@ -253,20 +221,20 @@ export default {
         getIssuePriority(),
         getProjectUserList(this.projectId),
         getProjectVersion(this.projectId)
-      ]).then(res => {
-        this.issueStatusList = res[0].data.map(item => {
+      ]).then((res) => {
+        this.issueStatusList = res[0].data.map((item) => {
           return { label: item.name, value: item.id }
         })
-        this.issueTypeList = res[1].data.map(item => {
+        this.issueTypeList = res[1].data.map((item) => {
           return { label: item.name, value: item.id }
         })
-        this.issuePriorityList = res[2].data.map(item => {
+        this.issuePriorityList = res[2].data.map((item) => {
           return { label: item.name, value: item.id }
         })
-        this.issueAssigneeList = res[3].data.user_list.map(item => {
+        this.issueAssigneeList = res[3].data.user_list.map((item) => {
           return { label: item.name, value: item.id }
         })
-        this.issueVersionList = res[4].data.versions.map(item => {
+        this.issueVersionList = res[4].data.versions.map((item) => {
           return { label: item.name, value: item.id }
         })
       })
@@ -275,31 +243,50 @@ export default {
       this.$emit('add-topic-visible', false)
     },
     handleSave() {
-      this.$refs['issueForm'].validate(async(valid) => {
+      this.$refs['issueForm'].validate(async (valid) => {
         if (valid) {
           const data = this.issueForm
-          if (this.issueForm.fixed_version_id === '') {
-            delete this.issueForm.fixed_version_id
+          const filetype = this.extension[this.uploadFileList[0].raw.type]
+          const form = new FormData()
+          form.append('project_id', this.projectId)
+          Object.keys(data).forEach((objKey) => {
+            if (objKey === 'fixed_version_id' && data['fixed_version_id'] === '') return false
+            if (objKey === 'parent_id' && (data['parent_id'] === '' || data['parent_id'] === 0)) return false
+            form.append(objKey, data[objKey])
+          })
+          if (this.uploadFileList.length > 0) {
+            form.append('upload_file', this.uploadFileList[0].raw, this.uploadFileList[0].raw.name)
           }
-          if (this.parentid !== 0) {
-            data['parent_id'] = this.parentid
-          } else {
-            delete data['parent_id']
-          }
-          data['project_id'] = this.projectId
           this.LoadingConfirm = true
-          await this.saveData(data)
+          await this.saveData(form)
           this.LoadingConfirm = false
         } else {
           return false
         }
       })
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(`Only one file can be added at a time, please delete the existing file first`)
+    },
+    async handleChange(file, fileList) {
+      if (this.extension[file.raw.type] === undefined) {
+        this.$message.warning(`Unable to upload a file: This file type is not supported`)
+        this.$refs['upload'].clearFiles()
+      } else if (file.size / 1024 / 1024 > 5) {
+        this.$message.warning(`This file cannot be uploaded because it exceeds the maximum allowed file size (5 MB)`)
+        this.$refs['upload'].clearFiles()
+      } else {
+        this.uploadFileList = fileList
+      }
     }
   }
 }
 </script>
 <style lang="scss">
-  .el-upload-dragger {
-    height: 50px;
-  }
+.el-upload-dragger {
+  height: 50px;
+}
+.el-upload__text {
+  margin-top: 18px;
+}
 </style>
