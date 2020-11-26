df160914928aaab258cda5023c2e2019211fed31
kevin chen <yha0971@gmail.com>
2020-11-26 19:06:32 +0800

    feat: pm project list design

--- src/views/ProjectListPM/index.vue
+++ src/views/ProjectListPM/index.vue
@@ -261,7 +261,7 @@ export default {
     </div>
     <el-divider />
     <el-table v-loading="listLoading" :data="pagedData" element-loading-text="Loading" border fit highlight-current-row>
-      <el-table-column label="Name / Identifier" :show-overflow-tooltip="true">
+      <el-table-column label="Name" :show-overflow-tooltip="true">
         <template slot-scope="scope">
           <!-- <router-link
             :to="{
@@ -269,11 +269,14 @@ export default {
             }"
             style="color: #409EFF"
           > -->
-          <span style="color: #67c23a">{{ scope.row.display }}</span>
-          <br>
-          <span style="color: #409eff">{{ scope.row.name }}</span>
+          {{ scope.row.display }}
           <!-- </router-link> -->
           <!-- <span>{{ scope.row.name }}</span> -->
+        </template>
+      </el-table-column>
+      <el-table-column label="Identifier">
+        <template slot-scope="scope">
+          <span style="color: #949494; font-weight: 400">#{{ scope.row.name }}</span>
         </template>
       </el-table-column>
       <el-table-column align="center" label="Status" width="120">
@@ -315,13 +318,13 @@ export default {
           {{ scope.row.updated_time | relativeTime }}
         </template>
       </el-table-column>
-      <el-table-column align="center" label="GitLab" width="100px">
+      <el-table-column align="center" label="GitLab" width="120px">
         <template slot-scope="scope">
           <el-link v-if="scope.row.git_url" type="primary" :href="scope.row.git_url" target="_blank">GitLab</el-link>
           <span v-else>-</span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="Redmine" width="100px">
+      <el-table-column align="center" label="Redmine" width="120px">
         <template slot-scope="scope">
           <el-link
             v-if="scope.row.redmine_url"
