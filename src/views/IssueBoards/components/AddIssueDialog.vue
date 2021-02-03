35bb106e21adef4bc9c73c7367b0ba04c2c32d92
Asher Lin <thegaze77@gmail.com>
2021-02-03 14:26:47 +0800

    feat: add assignee form item

--- src/views/IssueBoards/components/AddIssueDialog.vue
+++ src/views/IssueBoards/components/AddIssueDialog.vue
@@ -1,6 +1,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import { getIssueStatus, getIssueTracker, getIssuePriority } from '@/api/issue'
+import { getProjectAssignable } from '@/api/projects'
 import { addIssue } from '@/api/issue'
 import { Message } from 'element-ui'
 
@@ -21,6 +22,7 @@ export default {
       priority_id: '',
       tracker_id: '',
       status_id: '',
+      assigned_to_id: '',
       description: ''
     },
     issueFormRules: {
@@ -32,12 +34,16 @@ export default {
     issuePriorityList: [],
     issueTypeList: [],
     issueStatusList: [],
+    issueAssigneeList: [],
     isLoading: false
   }),
   computed: {
     ...mapGetters(['projectSelectedId'])
   },
   watch: {
+    projectSelectedId() {
+      this.fetchData()
+    },
     issueStatusList(val) {
       if (val) {
         this.issueForm.status_id = this.issueStatusList.find(i => i.label === this.focusStatus).value
@@ -49,16 +55,19 @@ export default {
   },
   methods: {
     fetchData() {
-      Promise.all([getIssuePriority(), getIssueTracker(), getIssueStatus()]).then(res => {
-        this.issuePriorityList = res[0].data.map(item => {
-          return { label: item.name, value: item.id }
-        })
-        this.issueTypeList = res[1].data.map(item => {
-          return { label: item.name, value: item.id }
-        })
-        this.issueStatusList = res[2].data.map(item => {
-          return { label: item.name, value: item.id }
-        })
+      Promise.all([
+        getIssuePriority(),
+        getIssueTracker(),
+        getIssueStatus(),
+        getProjectAssignable(this.projectSelectedId)
+      ]).then(res => {
+        this.issuePriorityList = res[0].data.map(item => ({ label: item.name, value: item.id }))
+        this.issueTypeList = res[1].data.map(item => ({ label: item.name, value: item.id }))
+        this.issueStatusList = res[2].data.map(item => ({ label: item.name, value: item.id }))
+        this.issueAssigneeList = res[3].data.user_list.map(item => ({
+          label: item.login,
+          value: item.id
+        }))
       })
     },
     handleClose() {
@@ -105,6 +114,11 @@ export default {
     <el-form ref="issueForm" :model="issueForm" :rules="issueFormRules" label-width="100px">
       <el-form-item :label="$t('general.Title')" prop="subject">
         <el-input id="input-name" v-model="issueForm.subject" :placeholder="$t('general.PleaseInput')" />
+      </el-form-item>
+      <el-form-item :label="$t('Issue.Assignee')" prop="assigned_to_id">
+        <el-select v-model="issueForm.assigned_to_id" style="width: 100%" clearable>
+          <el-option v-for="item in issueAssigneeList" :key="item.value" :label="item.label" :value="item.value" />
+        </el-select>
       </el-form-item>
       <el-form-item :label="$t('Issue.Priority')" prop="priority_id">
         <el-select v-model="issueForm.priority_id" style="width: 100%">
