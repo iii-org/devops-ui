b4b2c5bde0b51ca28fd7e493e6d864fdbd5dd76e
Asher Lin <thegaze77@gmail.com>
2021-03-30 09:58:31 +0800

    chore: hide plugin settings collapse item

--- src/views/ProjectSettings/index.vue
+++ src/views/ProjectSettings/index.vue
@@ -5,14 +5,15 @@
     </div>
     <el-divider />
     <el-row :gutter="10">
-      <el-col class="mb-4" :sm="24" :md="10" :lg="9">
+      <!-- <el-col class="mb-4" :sm="24" :md="10" :lg="9">
         <el-card shadow="never">
           <el-collapse v-model="activeNames">
             <PluginSettings />
           </el-collapse>
         </el-card>
-      </el-col>
-      <el-col class="mb-4" :sm="24" :md="14" :lg="15">
+      </el-col> -->
+      <!-- <el-col class="mb-4" :sm="24" :md="14" :lg="15"> -->
+      <el-col class="mb-4" :xs="24">
         <el-card shadow="never">
           <el-collapse v-model="activeNames">
             <ProjectVersions />
