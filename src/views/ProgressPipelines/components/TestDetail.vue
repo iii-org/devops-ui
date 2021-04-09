64442ee50942d38219fc6e5d3adf9b1ecc8efd5a
Asher Lin <thegaze77@gmail.com>
2021-04-09 16:17:42 +0800

    chore: socket connection error test

--- src/views/ProgressPipelines/components/TestDetail.vue
+++ src/views/ProgressPipelines/components/TestDetail.vue
@@ -5,7 +5,6 @@
         {{ $t('ProcessDevBranchTest.TestDetail') }}
       </span>
     </template>
-
     <el-tabs v-model="activeStage" tab-position="left" @tab-click="handleClick">
       <el-tab-pane
         v-for="(stage, idx) in stages"
@@ -13,13 +12,17 @@
         :name="`${idx + 1} ${stage.name}`"
         :disabled="!stage.state"
       >
-        <div slot="label">
-          <div class="d-flex justify-space-between align-center">
-            <span class="text-right">{{ idx + 1 }} {{ stage.name }}</span>
-            <el-tag v-if="stage.state" class="el-tag--circle ml-2" :type="getStateTagType(stage.state)" size="mini" effect="dark">
-              {{ stage.state }}
-            </el-tag>
-          </div>
+        <div slot="label" class="d-flex justify-space-between align-center">
+          <span class="text-right">{{ idx + 1 }} {{ stage.name }}</span>
+          <el-tag
+            v-if="stage.state"
+            class="el-tag--circle ml-2"
+            :type="getStateTagType(stage.state)"
+            size="mini"
+            effect="dark"
+          >
+            {{ stage.state }}
+          </el-tag>
         </div>
         <el-card
           :body-style="{
@@ -28,13 +31,11 @@
             lineHeight: 2
           }"
         >
-          <div class="d-flex justify-space-between mb-3">
-            <span class="text-h6">
-              <i class="el-icon-tickets mr-2" />
-              <span class="mr-3">{{ stage.name }}</span>
-              <i v-if="stage.isLoading" class="el-icon-loading font-weight-bold" style="color: #F89F03" />
-              <i v-else class="el-icon-check font-weight-bold" style="color: #72C040" />
-            </span>
+          <div class="text-h6 mb-3">
+            <i class="el-icon-tickets mr-2" />
+            <span class="mr-3">{{ stage.name }}</span>
+            <i v-if="stage.isLoading" class="el-icon-loading font-weight-bold" style="color: #F89F03" />
+            <i v-else class="el-icon-check font-weight-bold" style="color: #72C040" />
           </div>
           <el-card
             v-for="(step, stepIdx) in stage.steps"
@@ -96,17 +97,8 @@ export default {
         this.stages = val.map(item => item)
         this.activeStage = `1 ${this.stages[0].name}`
         this.handleClick({ index: 0 })
-        // this.fetchHarborLogByStep()
       }
     }
-    // stages: {
-    //   handler(val) {
-    //     if (val.some(item => item.isLoading === false)) {
-    //       console.log('Finished')
-    //     }
-    //   },
-    //   deep: true
-    // }
   },
   methods: {
     handleClose() {
@@ -127,41 +119,6 @@ export default {
         default:
           return 'slow'
       }
-    },
-    fetchHarborLogByStep() {
-      const manager = new Manager(process.env.VUE_APP_BASE_API, { reconnectionAttempts: 5 })
-      const { repository_id } = this.selectedProject
-      const socket = manager.socket('/rancher/websocket/logs')
-      socket.on('connect', () => {
-        // console.log('socket.connected', socket.connected)
-      })
-      this.stages.forEach((item, stageIdx) =>
-        item.steps.forEach((step, stepIdx) => {
-          const emitObj = {
-            repository_id,
-            pipelines_exec_run: this.pipelinesExecRun,
-            stage_index: item.stage_id,
-            step_index: step.step_id
-          }
-          socket.emit('get_pipe_log', emitObj)
-          // console.log('sioEmit ===>', { emitObj, stage_index: emitObj.stage_index, step_index: emitObj.step_index })
-          socket.on('pipeline_log', sioEvt => {
-            const { stage_index, step_index, data } = sioEvt
-            // console.log('sioEvt ===>', { sioEvt, stage_index, step_index, data })
-            if (data.length && stage_index - 1 === stageIdx && step_index === stepIdx) {
-              // this.$set(this.stages[stageIdx].steps[stepIdx], 'message', data)
-              this.stages[stageIdx].steps[stepIdx].message = data
-            } else if (!data.length && stage_index - 1 === stageIdx && step_index === stepIdx) {
-              // socket.disconnect()
-              this.stages[stageIdx].isLoading = false
-            }
-          })
-          socket.on('disconnect', () => {
-            // socket.disconnect()
-            // console.log('socket.disconnected', socket.disconnected)
-          })
-        })
-      )
     },
     handleClick(tab) {
       const index = Number(tab.index)
