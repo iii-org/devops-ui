1228d95a729ed4b86997e19f8cbff27b122440e6
Asher Lin <thegaze77@gmail.com>
2021-04-13 11:35:46 +0800

    fix: download webinspect report error

--- src/views/ScanWebInspect.vue
+++ src/views/ScanWebInspect.vue
@@ -1,10 +1,91 @@
+<template>
+  <div class="app-container">
+    <div class="d-flex justify-space-between">
+      <project-list-selector />
+      <el-input
+        v-model="searchData"
+        class="mr-3"
+        :placeholder="$t('WebInspect.SearchCommitId')"
+        style="width: 250px"
+        prefix-icon="el-icon-search"
+      />
+    </div>
+    <el-divider />
+    <el-table
+      v-loading="listLoading"
+      :element-loading-text="$t('Loading')"
+      border
+      fit
+      highlight-current-row
+      :data="pagedData"
+      height="100%"
+    >
+      <el-table-column
+        align="center"
+        :label="$t('WebInspect.ScanId')"
+        prop="scan_id"
+        min-width="120"
+        :show-overflow-tooltip="true"
+      />
+      <el-table-column align="center" :label="$t('WebInspect.Branch')" prop="branch" min-width="120" />
+      <el-table-column align="center" :label="$t('WebInspect.Commit')" prop="commit_id" min-width="100">
+        <template slot-scope="scope">
+          <el-link
+            type="primary"
+            target="_blank"
+            style="font-size: 16px"
+            :underline="false"
+            :href="scope.row.issue_link"
+          >
+            {{ scope.row.commit_id }}
+          </el-link>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('WebInspect.Status')" prop="stats.status" min-width="130">
+        <template slot-scope="scope">
+          <el-tag
+            v-if="scope.row.stats.status"
+            class="el-tag--circle"
+            :type="getStatusTagType(scope.row.stats.status)"
+            effect="dark"
+          >
+            {{ scope.row.stats.status }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('WebInspect.Critical')" prop="stats.4" />
+      <el-table-column align="center" :label="$t('WebInspect.HighSeverity')" prop="stats.3" />
+      <el-table-column align="center" :label="$t('WebInspect.MediumSeverity')" prop="stats.2" />
+      <el-table-column align="center" :label="$t('WebInspect.LowSeverity')" prop="stats.1" />
+      <el-table-column align="center" :label="$t('WebInspect.InfoSeverity')" prop="stats.0" />
+      <el-table-column-time :label="$t('WebInspect.RunAt')" prop="run_at" />
+      <el-table-column align="center" :label="$t('WebInspect.Report')" min-width="100">
+        <template slot-scope="scope">
+          <el-link
+            type="primary"
+            style="font-size: 16px"
+            :disabled="!scope.row.scan_id || scope.row.stats.status !== 'Complete'"
+            :underline="false"
+            @click="fetchTestReport(scope.row.scan_id)"
+          >
+            <i class="el-icon-download" style="font-size: 16px" />
+          </el-link>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination
+      :total="filteredData.length"
+      :page="listQuery.page"
+      :limit="listQuery.limit"
+      :page-sizes="[listQuery.limit]"
+      :layout="'total, prev, pager, next'"
+      @pagination="onPagination"
+    />
+  </div>
+</template>
+
 <script>
-import {
-  getWebInspectReport,
-  getWebInspectScans,
-  getWebInspectStats,
-  getWebInspectStatus
-} from '@/api/webInspect'
+import { getWebInspectReport, getWebInspectScans, getWebInspectStats, getWebInspectStatus } from '@/api/webInspect'
 import MixinElTableWithAProject from '@/components/MixinElTableWithAProject'
 import ElTableColumnTime from '@/components/ElTableColumnTime'
 
@@ -89,70 +170,3 @@ export default {
   }
 }
 </script>
-
-<template>
-  <div class="app-container">
-    <div class="d-flex justify-space-between">
-      <project-list-selector />
-      <el-input
-        v-model="searchData"
-        class="mr-3"
-        :placeholder="$t('WebInspect.SearchCommitId')"
-        style="width: 250px"
-        prefix-icon="el-icon-search"
-      />
-    </div>
-    <el-divider />
-    <el-table v-loading="listLoading" :element-loading-text="$t('Loading')" border fit highlight-current-row :data="pagedData" height="100%">
-      <el-table-column align="center" :label="$t('WebInspect.ScanId')" prop="scan_id" min-width="120" :show-overflow-tooltip="true" />
-      <el-table-column align="center" :label="$t('WebInspect.Branch')" prop="branch" min-width="120" />
-      <el-table-column align="center" :label="$t('WebInspect.Commit')" prop="commit_id" min-width="100">
-        <template slot-scope="scope">
-          <el-link
-            type="primary"
-            target="_blank"
-            style="font-size: 16px"
-            :underline="false"
-            :href="scope.row.issue_link"
-          >
-            {{ scope.row.commit_id }}
-          </el-link>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" :label="$t('WebInspect.Status')" prop="stats.status" min-width="130">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.stats.status" class="el-tag--circle" :type="getStatusTagType(scope.row.stats.status)" effect="dark">
-            {{ scope.row.stats.status }}
-          </el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" :label="$t('WebInspect.Critical')" prop="stats.4" />
-      <el-table-column align="center" :label="$t('WebInspect.HighSeverity')" prop="stats.3" />
-      <el-table-column align="center" :label="$t('WebInspect.MediumSeverity')" prop="stats.2" />
-      <el-table-column align="center" :label="$t('WebInspect.LowSeverity')" prop="stats.1" />
-      <el-table-column align="center" :label="$t('WebInspect.InfoSeverity')" prop="stats.0" />
-      <el-table-column-time :label="$t('WebInspect.RunAt')" prop="run_at" />
-      <el-table-column align="center" :label="$t('WebInspect.Report')" min-width="100">
-        <template slot-scope="scope">
-          <el-link
-            type="primary"
-            style="font-size: 16px"
-            :disabled="!scope.row.scan_id"
-            :underline="false"
-            @click="fetchTestReport(scope.row.scan_id)"
-          >
-            <i class="el-icon-download" style="font-size: 16px" />
-          </el-link>
-        </template>
-      </el-table-column>
-    </el-table>
-    <pagination
-      :total="filteredData.length"
-      :page="listQuery.page"
-      :limit="listQuery.limit"
-      :page-sizes="[listQuery.limit]"
-      :layout="'total, prev, pager, next'"
-      @pagination="onPagination"
-    />
-  </div>
-</template>
