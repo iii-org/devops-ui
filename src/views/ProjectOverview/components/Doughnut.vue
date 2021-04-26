c4cb45a5567301fbab097631543964858bd2e0dc
Clay <noitseuq.rm@gmail.com>
2021-04-26 15:51:03 +0800

    feat: add new Doughnut chart by echart

---
+++ src/views/ProjectOverview/components/Doughnut.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-chart
+    class="chart"
+    :option="option"
+    theme="macarons"
+    autoresize
+  />
+</template>
+
+<script>
+import VChart from 'vue-echarts'
+import { use } from 'echarts/core'
+import { TooltipComponent, LegendComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+import { PieChart } from 'echarts/charts'
+
+require('echarts/theme/macarons') // echarts theme
+use([TooltipComponent, LegendComponent, PieChart, CanvasRenderer])
+
+export default {
+  name: 'Doughnut',
+  components: {
+    'v-chart': VChart
+  },
+  props: {
+    chartData: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    option() {
+      return {
+        tooltip: {
+          trigger: 'item',
+          backgroundColor: '#ffffff'
+        },
+        legend: {
+          orient: 'vertical',
+          top: '25%',
+          left: 'left'
+        },
+        color: this.chartData.color,
+        series: [
+          {
+            name: '',
+            type: 'pie',
+            radius: ['50%', '95%'],
+            center: ['55%', '50%'],
+            avoidLabelOverlap: false,
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '40',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: this.chartData.data
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart {
+  height: 100%;
+  width: 100%;
+  min-height: 350px;
+}
+</style>
--- src/views/ProjectOverview/components/IssueStatusCard.vue
+++ src/views/ProjectOverview/components/IssueStatusCard.vue
@@ -14,7 +14,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import Doughnut from './Doughnut.js'
+import Doughnut from './Doughnut.vue'
 
 export default {
   name: 'IssueStatusCard',
@@ -47,22 +47,21 @@ export default {
     },
     fillData(chartData) {
       const issueStatusList = [
-        { label: this.$t('Issue.Active'), color: '#56b1e8', key: 'Active' },
-        { label: this.$t('Issue.Assigned'), color: '#5375bf', key: 'Assigned' },
-        { label: this.$t('Issue.Closed'), color: '#c4cfcf', key: 'Closed' },
-        { label: this.$t('Issue.Solved'), color: '#3ecbbc', key: 'Solved' },
-        { label: this.$t('Issue.Responded'), color: '#ec8539', key: 'Responded' },
-        { label: this.$t('Issue.Finished'), color: '#53bf64', key: 'Finished' }
+        { label: this.$t('Issue.Active'), color: '#409EFF', key: 'Active' },
+        { label: this.$t('Issue.Assigned'), color: '#F56C6C', key: 'Assigned' },
+        { label: this.$t('Issue.Closed'), color: '#909399', key: 'Closed' },
+        { label: this.$t('Issue.Solved'), color: '#3ECBBC', key: 'Solved' },
+        { label: this.$t('Issue.Responded'), color: '#E6A23C', key: 'Responded' },
+        { label: this.$t('Issue.Finished'), color: '#67C23A', key: 'Finished' }
         // { label: this.$t('Issue.Unknown'), color: 'red', key: 'Unknown' }
       ]
+
       this.dataCollection = {
-        labels: issueStatusList.map(status => status.label),
-        datasets: [
-          {
-            data: issueStatusList.map(status => status.key).map(key => chartData[key]),
-            backgroundColor: issueStatusList.map(status => status.color)
-          }
-        ]
+        color: issueStatusList.map(item => item.color),
+        data: issueStatusList.map(status => ({
+          value: chartData[status.key],
+          name: status.label
+        }))
       }
     }
   }
