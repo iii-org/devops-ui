08d27e821212b564f6a754c2379731bc4f6d22f7
Asher Lin <thegaze77@gmail.com>
2021-04-29 12:04:34 +0800

    fix: pipeline update time format

--- src/views/ProgressPipelines/index.vue
+++ src/views/ProgressPipelines/index.vue
@@ -150,8 +150,8 @@ export default {
       }
       const rid = this.selectedProject.repository_id
       try {
-        this.lastUpdateTime = this.$dayjs().format('YYYY-MM-DD hh:mm:ss')
         const pipe = await getPipelines(rid)
+        this.lastUpdateTime = this.$dayjs(pipe.datetime).utcOffset(16).format('YYYY-MM-DD hh:mm:ss')
         pipe.data.forEach((item, idx) => {
           const result = { ...item }
           if (result.execution_state === 'Success') result.execution_state = 'Finished'
