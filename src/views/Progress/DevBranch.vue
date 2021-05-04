998fae39b05891b8ecb7bfff07a8b3dde1be0cde
Asher Lin <thegaze77@gmail.com>
2021-05-04 13:42:20 +0800

    fix: progress dev branch page fetch data error

--- src/views/Progress/DevBranch.vue
+++ src/views/Progress/DevBranch.vue
@@ -1,52 +1,50 @@
 <template>
   <el-row class="app-container">
-    <el-col>
-      <div class="d-flex justify-space-between">
-        <project-list-selector />
-        <el-input
-          v-model="searchData"
-          style="width: 250px"
-          prefix-icon="el-icon-search"
-          :placeholder="$t('ProcessDevBranch.SearchBranch')"
-        />
-      </div>
-      <el-divider />
-      <el-table v-loading="listLoading" :data="pagedData" :element-loading-text="$t('Loading')" fit border>
-        <el-table-column align="center" :label="$t('ProcessDevBranch.Branch')" min-width="160" prop="name" />
-        <el-table-column align="center" :label="$t('general.Description')" min-width="160" prop="last_commit_message" />
-        <el-table-column align="center" :label="$t('ProcessDevBranch.Environment')" min-width="200">
-          <template slot-scope="scope">
-            <div v-for="(item, index) in EnvironmentFormat(scope.row.env_url)" :key="index">
-              <el-link type="primary" target="_blank" style="font-size: 16px" :underline="false" :href="item.url">
-                {{ item.service }} ({{ item.port }})
-              </el-link>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" :label="$t('ProcessDevBranch.Commit')" min-width="100" prop="short_id">
-          <template slot-scope="scope">
-            <el-link
-              type="primary"
-              target="_blank"
-              style="font-size: 16px"
-              :underline="false"
-              :href="scope.row.commit_url"
-            >
-              {{ scope.row.short_id }}
-            </el-link>
-          </template>
-        </el-table-column>
-        <el-table-column-time prop="last_commit_time" />
-      </el-table>
-      <pagination
-        :total="filteredData.length"
-        :page="listQuery.page"
-        :limit="listQuery.limit"
-        :page-sizes="[listQuery.limit]"
-        :layout="'total, prev, pager, next'"
-        @pagination="onPagination"
+    <div class="d-flex justify-space-between">
+      <project-list-selector />
+      <el-input
+        v-model="searchData"
+        :placeholder="$t('ProcessDevBranch.SearchBranch')"
+        style="width: 250px"
+        prefix-icon="el-icon-search"
       />
-    </el-col>
+    </div>
+    <el-divider />
+    <el-table v-loading="listLoading" :data="pagedData" :element-loading-text="$t('Loading')" border fit height="100%">
+      <el-table-column align="center" :label="$t('ProcessDevBranch.Branch')" min-width="160" prop="name" />
+      <el-table-column align="center" :label="$t('general.Description')" min-width="160" prop="last_commit_message" />
+      <el-table-column align="center" :label="$t('ProcessDevBranch.Environment')" min-width="200">
+        <template slot-scope="scope">
+          <div v-for="(item, index) in formatEnvironment(scope.row.env_url)" :key="index">
+            <el-link type="primary" target="_blank" style="font-size: 16px" :underline="false" :href="item.url">
+              {{ item.service }} ({{ item.port }})
+            </el-link>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('ProcessDevBranch.Commit')" min-width="100" prop="short_id">
+        <template slot-scope="scope">
+          <el-link
+            type="primary"
+            target="_blank"
+            style="font-size: 16px"
+            :underline="false"
+            :href="scope.row.commit_url"
+          >
+            {{ scope.row.short_id }}
+          </el-link>
+        </template>
+      </el-table-column>
+      <el-table-column-time prop="last_commit_time" />
+    </el-table>
+    <pagination
+      :total="filteredData.length"
+      :page="listQuery.page"
+      :limit="listQuery.limit"
+      :page-sizes="[listQuery.limit]"
+      :layout="'total, prev, pager, next'"
+      @pagination="onPagination"
+    />
   </el-row>
 </template>
 
@@ -78,18 +76,10 @@ export default {
       await this.getBranchesByProject(repository_id)
       return this.branchList
     },
-    returnTagType(row) {
-      const { success, total } = row.last_test_result
-      return success === total ? 'success' : 'danger'
-    },
-    testResults(row) {
-      const { success, total } = row.last_test_result
-      return success + ' / ' + total
-    },
     onPagination(listQuery) {
       this.listQuery = listQuery
     },
-    EnvironmentFormat(env) {
+    formatEnvironment(env) {
       const serviceArray = Object.keys(env)
       const environmentArray = []
       serviceArray.forEach(function(item) {
