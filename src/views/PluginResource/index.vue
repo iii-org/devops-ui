77212efd50b1cebf0ffa6e74bf2da7c36bce9f07
Clay <noitseuq.rm@gmail.com>
2021-05-06 13:49:35 +0800

    feat: modify PluginResource page looks like k8s

--- src/views/PluginResource/index.vue
+++ src/views/PluginResource/index.vue
@@ -1,36 +1,40 @@
 <template>
-  <div class="app-container">
-    <div class="d-flex justify-start">
-      <project-list-selector />
-    </div>
-    <el-divider />
-    <el-row :gutter="12">
-      <el-col v-for="item in usageList" :key="item.title" :lg="8" :xl="6">
-        <el-card v-loading="listLoading" class="mb-4">
-          <div slot="header" class="d-flex justify-space-between align-center" style="height: 24px">
-            <strong>{{ item.title }}{{ item.quota }}</strong>
-            <div>
-              <el-tooltip effect="dark" :content="`${item.title} Resource`" placement="top-start">
-                <el-button
-                  v-if="allowEditUsage(item.title)"
-                  type="primary"
-                  icon="el-icon-edit"
-                  size="mini"
-                  circle
-                  plain
-                  @click="handleEdit(item.title)"
-                />
-              </el-tooltip>
+  <el-row class="app-container">
+    <el-col>
+      <el-row>
+        <el-col>
+          <project-list-selector />
+        </el-col>
+      </el-row>
+      <el-divider />
+      <el-row :gutter="12">
+        <el-col v-for="item in usageList" :key="item.title" :lg="8" :xl="6">
+          <el-card
+            v-loading="listLoading"
+            :class="{ 'mb-3': true, 'float-card': hasDetail(item.title) }"
+            :shadow="hasDetail(item.title) ? 'always' : 'never'"
+            @click.native="showDetail(item.title)"
+          >
+            <div slot="header" class="d-flex justify-space-between align-center" style="height: 24px">
+              <strong>{{ item.title }}{{ item.quota }}</strong>
             </div>
-          </div>
-          <div v-if="selectedProjectId === -1" style="text-align: center;">{{ $t('general.NoData') }}</div>
-          <div v-else>
-            <resource-pie :chart-data="item.data" />
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-  </div>
+            <div v-if="selectedProjectId === -1" style="text-align: center;">{{ $t('general.NoData') }}</div>
+            <div v-else>
+              <div>
+                <resource-pie :chart-data="item.data" />
+              </div>
+              <div
+                :class="hasDetail(item.title) ? 'details-reminder' : 'reminder-space'"
+                :style="{ cursor: 'pointer' }"
+              >
+                {{ $t('ProjectResource.Details') }}
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
@@ -59,18 +63,18 @@ const formatChartData = item => {
     result.push({
       value: formatValue(item.title, item.used.value),
       name: `Usage (${getValueUnit(item.title)})`,
-      itemStyle: { color: '#E85656', emphasis: { color: '#E85656' } }
+      itemStyle: { color: '#E85656', emphasis: { color: '#E85656' }}
     })
   } else {
     result.push({
       value: formatValue(item.title, handleLeftQuota(item)),
       name: `Left Quota (${getValueUnit(item.title)})`,
-      itemStyle: { color: '#3ECBBC', emphasis: { color: '#3ECBBC' } }
+      itemStyle: { color: '#3ECBBC', emphasis: { color: '#3ECBBC' }}
     })
     result.push({
       value: formatValue(item.title, item.used.value),
       name: `Usage (${getValueUnit(item.title)})`,
-      itemStyle: { color: '#E85656', emphasis: { color: '#E85656' } }
+      itemStyle: { color: '#E85656', emphasis: { color: '#E85656' }}
     })
   }
   return result
@@ -118,13 +122,42 @@ export default {
       }
       this.listLoading = false
     },
-    handleEdit(target) {
-      this.$router.push({ name: target })
-    },
-    allowEditUsage(target) {
+    hasDetail(target) {
       const allowList = ['Harbor']
       return allowList.findIndex(i => i === target) > -1
+    },
+    showDetail(target) {
+      if (!this.hasDetail(target)) {
+        return
+      }
+      this.$router.push({ name: `${target}` })
     }
   }
 }
 </script>
+
+<style scoped>
+.float-card:not(:hover) {
+  border: transparent solid 2px;
+}
+
+.float-card:hover {
+  border: red solid 2px;
+}
+
+.details-reminder {
+  font-weight: bold;
+  color: #3358c3;
+  text-decoration: underline;
+  font-size: 19px;
+  text-align: end;
+}
+
+.reminder-space {
+  font-weight: bold;
+  color: transparent;
+  text-decoration: underline;
+  font-size: 19px;
+  text-align: end;
+}
+</style>
