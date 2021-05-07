5a40b89450078d32b23b5a8b88ef18497e2449a2
yuan <13202145+becory@users.noreply.github.com>
2021-05-07 15:35:35 +0800

    feat: New Issue List and Filter (API change pending)

--- src/views/Project/IssueList/index.vue
+++ src/views/Project/IssueList/index.vue
@@ -1,146 +1,148 @@
 <template>
   <div class="app-container">
     <router-view />
-    <div v-if="this.$route.meta.rolePage" class="role-Page">
-      <div class="clearfix">
-        <div>
-          <project-list-selector />
-          <!--          <el-select-->
-          <!--            v-model="versionValue"-->
-          <!--            :placeholder="$t('Version.SelectVersion')"-->
-          <!--            :disabled="selectedProjectId === -1"-->
-          <!--            class="mr-4"-->
-          <!--            filterable-->
-          <!--            @change="updateData"-->
-          <!--          >-->
-          <!--            <el-option :key="-1" :label="$t('Dashboard.TotalVersion')" :value="'-1'" />-->
-          <!--            <el-option v-for="item in fixed_version" :key="item.id" :label="item.name" :value="item.id" />-->
-          <!--          </el-select>-->
-          <!--          <el-select-->
-          <!--            v-model="trackerValue"-->
-          <!--            :placeholder="$t('Issue.SelectType')"-->
-          <!--            :disabled="selectedProjectId === -1"-->
-          <!--            class="mr-4"-->
-          <!--            filterable-->
-          <!--            clearable-->
-          <!--            @change="updateData"-->
-          <!--          >-->
-          <!--            <el-option v-for="item in tracker" :key="item.id" :label="$t('Issue.'+item.name)" :value="item.id">-->
-          <!--              <tracker :name="item.name" />-->
-          <!--            </el-option>-->
-          <!--          </el-select>-->
-          <el-button
-            id="btn-add-issue"
-            type="success"
-            icon="el-icon-plus"
-            :disabled="selectedProjectId === -1"
-            @click="handleAddNewIssue"
-          >
-            {{ $t('Issue.AddIssue') }}
-          </el-button>
-          <el-input
-            id="input-search"
-            v-model="searchData"
-            prefix-icon="el-icon-search"
-            :placeholder="$t('Issue.SearchNameOrAssignee')"
-            style="width: 250px; float: right"
-          />
-        </div>
+    <div class="clearfix">
+      <div>
+        <project-list-selector />
+        <el-select
+          v-model="versionValue"
+          :placeholder="$t('Version.SelectVersion')"
+          :disabled="selectedProjectId === -1"
+          class="mr-4"
+          filterable
+          @change="updateData"
+        >
+          <el-option :key="-1" :label="$t('Dashboard.TotalVersion')" :value="'-1'" />
+          <el-option v-for="item in fixed_version" :key="item.id" :label="item.name" :value="item.id" />
+        </el-select>
+        <el-select
+          v-model="trackerValue"
+          :placeholder="$t('Issue.SelectType')"
+          :disabled="selectedProjectId === -1"
+          class="mr-4"
+          filterable
+          clearable
+          @change="updateData"
+        >
+          <el-option v-for="item in tracker" :key="item.id" :label="$t('Issue.'+item.name)" :value="item.id">
+            <tracker :name="item.name" />
+          </el-option>
+        </el-select>
+        <el-button
+          id="btn-add-issue"
+          type="success"
+          icon="el-icon-plus"
+          :disabled="selectedProjectId === -1"
+          @click="handleAddNewIssue"
+        >
+          {{ $t('Issue.AddIssue') }}
+        </el-button>
+        <el-input
+          id="input-search"
+          v-model="searchData"
+          prefix-icon="el-icon-search"
+          :placeholder="$t('Issue.SearchNameOrAssignee')"
+          style="width: 250px; float: right"
+        />
       </div>
-      <el-divider />
-      <el-table
-        v-loading="listLoading"
-        :data="pagedData"
-        :element-loading-text="$t('Loading')"
-        border
-        fit
-        highlight-current-row
-        row-key="id"
-        default-expand-all
-        :tree-props="{ children: 'children' }"
-        height="100%"
-      >
-        <el-table-column :label="$t('Issue.Id')" min-width="280" show-overflow-tooltip>
-          <template slot-scope="scope">
-            <span :class="isParentIssue(scope.row) ? 'ml-6' : ''">
-              <el-tooltip effect="dark" :content="$t('Issue.AddSubIssue')" placement="bottom-start" :open-delay="1000">
-                <el-button
-                  v-if="parentList.includes(scope.row.id) && scope.row.status.name !== 'Closed'"
-                  :id="`btn-add-sub-issue-${scope.$index}`"
-                  icon="el-icon-plus"
-                  type="text"
-                  circle
-                  plain
-                  size="mini"
-                  @click="handleParent(scope.$index, scope.row, scope)"
-                />
-              </el-tooltip>
-              <el-tooltip effect="dark" :content="$t('Issue.EditIssue')" placement="bottom-start" :open-delay="1000">
-                <el-button
-                  :id="`link-issue-name-${scope.$index}`"
-                  class="mr-1"
-                  type="primary"
-                  circle
-                  plain
-                  size="mini"
-                  icon="el-icon-edit"
-                  @click="handleEdit(scope.$index, scope.row)"
-                />
-              </el-tooltip>
-              <span class="text-success mr-2">{{ scope.row.id }}</span>
-              <span
-                class="font-weight-regular"
-                :style="{ 'font-size': '16px', cursor: 'pointer' }"
-                :underline="false"
-                @click="handleEdit(scope.$index, scope.row)"
-              >
-                {{ scope.row.name }}
-              </span>
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column :label="$t('general.Type')" width="130">
-          <template slot-scope="scope">
-            <tracker v-if="scope.row.tracker.name" :name="scope.row.tracker.name" />
-          </template>
-        </el-table-column>
-        <el-table-column align="center" :label="$t('general.Status')" width="150">
-          <template slot-scope="scope">
-            <status
-              v-if="scope.row.status.name"
-              :name="scope.row.status.name"
-            />
-          </template>
-        </el-table-column>
-        <el-table-column align="center" :label="$t('Issue.Assignee')" min-width="180">
-          <template slot-scope="scope">
-            <span>{{ scope.row.assigned_to.name }}</span>
-            <span v-if="scope.row.assigned_to.login">({{ scope.row.assigned_to.login }})</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" :label="$t('Issue.Priority')" width="150">
-          <template slot-scope="scope">
-            <priority v-if="scope.row.priority.name" :name="scope.row.priority.name" />
-          </template>
-        </el-table-column>
-      </el-table>
-      <pagination
-        :total="filteredData.length"
-        :page="listQuery.page"
-        :limit="listQuery.limit"
-        :page-sizes="[listQuery.limit]"
-        :layout="'total, prev, pager, next'"
-        @pagination="onPagination"
-      />
-      <add-issue
-        :save-data="saveIssue"
-        :dialog-visible.sync="addTopicDialogVisible"
-        :project-id="selectedProjectId"
-        :parent-id="parentId"
-        :parent-name="parentName"
-        @add-topic-visible="emitAddTopicDialogVisible"
-      />
     </div>
+    <el-divider />
+    <el-table
+      v-loading="listLoading"
+      :data="pagedData"
+      :element-loading-text="$t('Loading')"
+      border
+      fit
+      highlight-current-row
+      row-key="id"
+      :tree-props="{ children: 'child' }"
+      height="100%"
+      :row-class-name="getRowClass"
+    >
+      <el-table-column type="expand" class-name="informationExpand">
+        <template slot-scope="scope">
+          <div v-if="scope.row.parent_id">
+            <b>父議題:</b>
+            <el-link
+              class="font-weight-regular"
+              :style="{ 'font-size': '14px', cursor: 'pointer' }"
+              :underline="false"
+              @click="handleEdit(scope.row.parent_id)"
+            >
+              #{{ scope.row.parent_id }}
+            </el-link>
+          </div>
+          <div v-if="scope.row.children.length">
+            <b>子議題:</b>
+            <ol>
+              <li v-for="child in scope.row.children" :key="child.id">
+                <el-link
+                  class="font-weight-regular"
+                  :style="{ 'font-size': '14px', cursor: 'pointer' }"
+                  :underline="false"
+                  @click="handleEdit(child.id)"
+                >
+                  <status :name="child.status.name" size="mini" />  <tracker :name="child.tracker.name" /> #{{ child.id }} - {{ child.name }}
+                  <span v-if="Object.keys(child.assigned_to).length>1">({{ $t('Issue.Assignee') }}: {{ child.assigned_to.name }})</span>
+                </el-link>
+              </li>
+            </ol>
+          </div></template>
+      </el-table-column>
+      <el-table-column :label="$t('general.Type')" width="130">
+        <template slot-scope="scope">
+          <tracker v-if="scope.row.tracker.name" :name="scope.row.tracker.name" />
+        </template>
+      </el-table-column>
+      <el-table-column :label="$t('Issue.Id')" min-width="280" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <span class="text-success mr-2">#{{ scope.row.id }}</span>
+          <el-link
+            class="font-weight-regular"
+            :style="{ 'font-size': '16px', cursor: 'pointer' }"
+            :underline="false"
+            @click="handleEdit(scope.row.id)"
+          >
+            {{ scope.row.name }}
+          </el-link>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('Issue.Priority')" width="150">
+        <template slot-scope="scope">
+          <priority v-if="scope.row.priority.name" :name="scope.row.priority.name" />
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('general.Status')" width="150">
+        <template slot-scope="scope">
+          <status
+            v-if="scope.row.status.name"
+            :name="scope.row.status.name"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column align="center" :label="$t('Issue.Assignee')" min-width="180">
+        <template slot-scope="scope">
+          <span>{{ scope.row.assigned_to.name }}</span>
+          <span v-if="scope.row.assigned_to.login">({{ scope.row.assigned_to.login }})</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination
+      :total="filteredData.length"
+      :page="listQuery.page"
+      :limit="listQuery.limit"
+      :page-sizes="[listQuery.limit]"
+      :layout="'total, prev, pager, next'"
+      @pagination="onPagination"
+    />
+    <add-issue
+      :save-data="saveIssue"
+      :dialog-visible.sync="addTopicDialogVisible"
+      :project-id="selectedProjectId"
+      :parent-id="parentId"
+      :parent-name="parentName"
+      @add-topic-visible="emitAddTopicDialogVisible"
+    />
   </div>
 </template>
 
@@ -173,8 +175,7 @@ export default {
     trackerValue: '',
     fixed_version: [],
     tracker: [],
-    listFilterVersionTrackerData: [],
-    parentList: []
+    listFilterVersionTrackerData: []
   }),
   computed: {
     ...mapGetters(['userRole', 'userName']),
@@ -189,41 +190,6 @@ export default {
           data.assigned_to.name.toLowerCase().includes(this.searchData.toLowerCase())
         ) {
           return data
-        }
-
-        // Sub issue level 1
-        if (data.children.length > 0) {
-          const children1 = data.children.filter(dataChildren1 => {
-            if (Object.keys(dataChildren1.assigned_to).length <= 0) {
-              dataChildren1.assigned_to.name = ''
-            }
-            if (
-              dataChildren1.name.toLowerCase().includes(this.searchData.toLowerCase()) ||
-              dataChildren1.assigned_to.name.toLowerCase().includes(this.searchData.toLowerCase())
-            ) {
-              return dataChildren1
-            }
-            // Sub issue level 2
-            if (dataChildren1.children.length > 0) {
-              const children2 = dataChildren1.children.filter(dataChildren2 => {
-                if (Object.keys(dataChildren2.assigned_to).length <= 0) {
-                  dataChildren2.assigned_to.name = ''
-                }
-                if (
-                  dataChildren2.name.toLowerCase().includes(this.searchData.toLowerCase()) ||
-                  dataChildren2.assigned_to.name.toLowerCase().includes(this.searchData.toLowerCase())
-                ) {
-                  return dataChildren2
-                }
-              })
-              if (children2.length > 0) {
-                return data
-              }
-            }
-          })
-          if (children1.length > 0) {
-            return data
-          }
         }
       })
     }
@@ -254,21 +220,12 @@ export default {
         this.showNoProjectWarning()
         return []
       }
-
-      const data = (await getProjectIssueListByTree(this.selectedProjectId)).data
-      this.parentList = []
+      // TODO: 換成issue_list，不要使用tree的結構
+      const data = this.createRelativeList((await getProjectIssueListByTree(this.selectedProjectId)).data)
       const versionsRes = await getProjectVersion(this.selectedProjectId)
       this.fixed_version = [{ name: '版本未定', id: '' }, ...versionsRes.data.versions]
       const issueTrackerRes = await getIssueTracker()
       this.tracker = issueTrackerRes.data
-      data.forEach(item => {
-        this.parentList.push(item.id)
-        if (item.children.length !== 0) {
-          item.children.forEach(item2 => {
-            this.parentList.push(item2.id)
-          })
-        }
-      })
       if (this.userRole === 'Engineer') {
         this.searchData = this.userName
       }
@@ -308,13 +265,8 @@ export default {
         this.listFilterVersionTrackerData = res.map(items => items.item)
       }
     },
-    handleEdit(idx, row) {
-      this.$router.push({ name: 'issue-detail', params: { issueId: row.id }})
-    },
-    handleParent(idx, row) {
-      this.parentId = row.id
-      this.parentName = row.name
-      this.addTopicDialogVisible = true
+    handleEdit(id) {
+      this.$router.push({ name: 'issue-detail', params: { issueId: id }})
     },
     emitAddTopicDialogVisible(visible) {
       this.addTopicDialogVisible = visible
@@ -334,7 +286,21 @@ export default {
         .catch(error => {
           return error
         })
-      // this.addTopicDialogVisible = false
+    },
+    // TODO: 換成issue_list，不要使用tree的結構，之後可以刪掉
+    createRelativeList(list) {
+      const result = []
+
+      function flatList(parent) {
+        for (let i = 0; i < parent.length; i++) {
+          result.push(parent[i])
+          const children = parent[i].children
+          if (parent[i].children.length) flatList(children)
+        }
+      }
+
+      flatList(list)
+      return result
     },
     isParentIssue(row) {
       return row.parent_id === null && row.children.length === 0
@@ -342,10 +308,28 @@ export default {
     handleAddNewIssue() {
       this.addTopicDialogVisible = true
       this.parentId = 0
+    },
+    hasRelationIssue(row) {
+      return !!row.parent_id || (row.children !== undefined && row.children.length > 0)
+    },
+    getRowClass({ row }) {
+      if (this.hasRelationIssue(row) === false) {
+        return 'row-expand-cover'
+      } else {
+        return ''
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+>>>.row-expand-cover .el-table__expand-column .cell {
+  display:none;
+}
+>>>.el-table__expanded-cell{
+  font-size: 0.875em;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
 </style>
