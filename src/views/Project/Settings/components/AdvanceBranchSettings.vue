af818ab7cb7bd620245be63c79f99c68ad50294a
Asher Lin <thegaze77@gmail.com>
2021-05-19 22:13:25 +0800

    feat: add update advance branch settings buttons

--- src/views/Project/Settings/components/AdvanceBranchSettings.vue
+++ src/views/Project/Settings/components/AdvanceBranchSettings.vue
@@ -1,36 +1,50 @@
 <template>
   <el-row v-loading="isLoading" :element-loading-text="$t('Loading')" class="app-container">
     <el-col>
-      <router-link :to="{ name: 'Project Settings' }">
-        <svg-icon icon-class="system-uicons-exit-left" class="mr-2" />{{ $t('general.Exit') }}
-      </router-link>
-      <div class="d-flex justify-space-between align-center mt-2">
-        <div class="text-h6">{{ selectedProject.display }}</div>
-        <el-input
-          v-model="keyword"
-          size="medium"
-          prefix-icon="el-icon-search"
-          :style="{ width: '250px' }"
-          :placeholder="$t('general.SearchBranch')"
-        />
-      </div>
+      <el-row :gutter="10" type="flex" align="middle">
+        <el-col :span="2">
+          <router-link :to="{ name: 'Project Settings' }">
+            <svg-icon icon-class="system-uicons-exit-left" />{{ $t('general.Exit') }}
+          </router-link>
+        </el-col>
+        <el-col :span="16">
+          <span class="text-h6">{{ selectedProject.display }}</span>
+        </el-col>
+        <el-col :span="6" class="text-right">
+          <el-input
+            v-model="keyword"
+            size="small"
+            prefix-icon="el-icon-search"
+            :style="{ width: '250px' }"
+            :placeholder="$t('general.SearchBranch')"
+          />
+        </el-col>
+      </el-row>
+
       <el-divider />
-      <div class="text-right mb-2">
-        <el-button class="mr-1" @click="fetchPipelineBranch">{{ $t('general.Cancel') }}</el-button>
-        <el-button type="primary" @click="updatePipelineBranch">{{ $t('general.Save') }}</el-button>
+      <div class="text-right mb-3">
+        <el-button size="small" @click="handleReset">{{ $t('general.Cancel') }}</el-button>
+        <el-button type="primary" size="small" @click="updatePipelineBranch">{{ $t('general.Save') }}</el-button>
       </div>
-      <el-table :data="filteredData" fit>
+      <el-table :data="filteredData" fit border>
         <el-table-column :label="$t('Git.Branch')" align="center" prop="branch" :width="150" />
         <el-table-column :label="$t('general.Description')" align="center" prop="commit_message" />
         <el-table-column-time :label="$t('general.LastUpdateTime')" prop="commit_time" :min-width="80" />
         <el-table-column v-for="(tool, idx) in testingToolNames" :key="tool.name" align="center">
-          <template slot="header">
+          <template slot="header" slot-scope="scope">
             <div class="mb-2">{{ tool.name }}</div>
-            <!-- <div class="mb-2">{{ tool.checkAll }}</div> -->
-            <!-- <el-switch v-model="tool.checkAll" /> -->
+            <el-checkbox
+              v-if="listData.length > 1"
+              v-model="tool.selectedAll"
+              size="small"
+              @change="handleSelectAll(tool)"
+            />
           </template>
           <template slot-scope="scope">
-            <el-checkbox v-model="scope.row.testing_tools[idx].enable" />
+            <el-checkbox
+              v-model="scope.row.testing_tools[idx].enable"
+              @change="checkAllSelect(scope.row.testing_tools[idx], scope.row.branch)"
+            />
           </template>
         </el-table-column>
       </el-table>
@@ -39,9 +53,9 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { getPipelineBranch, editPipelineBranch } from '@/api/projects'
 import ElTableColumnTime from '@/components/ElTableColumnTime'
-import { mapGetters } from 'vuex'
 import MixinBasicTable from '@/mixins/MixinBasicTable'
 
 export default {
@@ -50,32 +64,33 @@ export default {
   mixins: [MixinBasicTable],
   data() {
     return {
+      services: ['Web', 'Database'],
+      dependenceKeys: ['Postman', 'WebInspect', 'ZAP', 'SideeX'],
       listData: [],
-      testingToolNames: [],
-      isAllSelected: [],
       isLoading: false,
       keyword: '',
       searchKeys: ['branch'],
+      testingToolNames: [],
       formChange: false
     }
   },
-  // beforeRouteLeave(to, from, next) {
-  //   if (this.formChange) {
-  //     this.$confirm(this.$t('Notify.UnSavedChanges'), this.$t('general.Warning'), {
-  //       confirmButtonText: this.$t('general.Confirm'),
-  //       cancelButtonText: this.$t('general.Cancel'),
-  //       type: 'warning'
-  //     })
-  //       .then(() => {
-  //         next()
-  //       })
-  //       .catch(() => {
-  //         next(false)
-  //       })
-  //   } else {
-  //     next()
-  //   }
-  // },
+  beforeRouteLeave(to, from, next) {
+    if (this.formChange) {
+      this.$confirm(this.$t('Notify.UnSavedChanges'), this.$t('general.Warning'), {
+        confirmButtonText: this.$t('general.Confirm'),
+        cancelButtonText: this.$t('general.Cancel'),
+        type: 'warning'
+      })
+        .then(() => {
+          next()
+        })
+        .catch(() => {
+          next(false)
+        })
+    } else {
+      next()
+    }
+  },
   computed: {
     ...mapGetters(['selectedProject']),
     selectedProjectRepositoryId() {
@@ -95,14 +110,6 @@ export default {
       })
     }
   },
-  watch: {
-    listData: {
-      handler() {
-        this.formChange = true
-      },
-      deep: true
-    }
-  },
   mounted() {
     this.fetchPipelineBranch()
   },
@@ -120,8 +127,8 @@ export default {
             testing_tools
           }
         })
-        this.testingToolNames = this.listData[0].testing_tools.map((tool, idx) => ({
-          checkAll: false,
+        this.testingToolNames = this.listData[0].testing_tools.map(tool => ({
+          selectedAll: this.checkAllSelected(tool.name),
           name: tool.name
         }))
       } catch (err) {
@@ -129,6 +136,13 @@ export default {
       } finally {
         this.isLoading = false
       }
+    },
+    checkAllSelected(name) {
+      const status = this.filteredData
+        .flatMap(i => i.testing_tools)
+        .filter(i => i.name === name)
+        .map(i => i.enable)
+      return status.every(i => i === true)
     },
     async updatePipelineBranch() {
       const sendData = {
@@ -153,6 +167,94 @@ export default {
       } finally {
         this.isLoading = false
       }
+    },
+    handleSelectAll(tool) {
+      this.formChange = true
+      const { selectedAll, name } = tool
+      this.filteredData.forEach(i =>
+        i.testing_tools.forEach(tool => {
+          if (tool.name === name) tool.enable = selectedAll
+        })
+      )
+      if (this.dependenceKeys.includes(name)) this.selectAllService(selectedAll)
+      if (this.services.includes(name)) this.selectAllDependence(selectedAll)
+    },
+    selectAllService(selectedAll) {
+      if (!selectedAll) return
+      const toolList = this.testingToolNames.map(tool => tool.name)
+      this.services.forEach(name => {
+        if (toolList.includes(name)) {
+          this.handleSelectAll({ selectedAll, name })
+          this.checkAllSelect({ name })
+        }
+      })
+    },
+    selectAllDependence(selectedAll) {
+      if (selectedAll) return
+      const toolList = this.testingToolNames.map(tool => tool.name)
+      this.dependenceKeys.forEach(name => {
+        if (toolList.includes(name)) {
+          this.handleSelectAll({ selectedAll, name })
+          this.checkAllSelect({ name })
+        }
+      })
+    },
+    checkAllSelect(tool, branch) {
+      const { name, enable } = tool
+      this.formChange = true
+      const status = this.filteredData
+        .flatMap(i => i.testing_tools)
+        .filter(i => i.name === name)
+        .map(i => i.enable)
+      const value = status.every(i => i === true)
+      const idx = this.testingToolNames.findIndex(i => i.name === name)
+      this.testingToolNames[idx].selectedAll = value
+      this.handleDependence(enable, name, branch)
+      if (this.dependenceKeys.includes(name)) this.checkServiceAllSelect()
+    },
+    checkServiceAllSelect() {
+      const toolList = this.testingToolNames.map(tool => tool.name)
+      this.services.forEach(name => {
+        if (toolList.includes(name)) {
+          this.checkAllSelect({ name })
+        }
+      })
+    },
+    checkDependenceAllSelect() {
+      const toolList = this.testingToolNames.map(tool => tool.name)
+      this.dependenceKeys.forEach(name => {
+        if (toolList.includes(name)) {
+          this.checkAllSelect({ name })
+        }
+      })
+    },
+    handleDependence(enable, name, branch) {
+      if (!branch) return
+      if (this.services.includes(name)) this.selectDependence(enable, branch)
+      if (this.dependenceKeys.includes(name)) this.selectServices(enable, branch)
+    },
+    selectDependence(enable, branch) {
+      if (enable) return
+      const idx = this.listData.findIndex(item => item.branch === branch)
+      this.dependenceKeys.forEach(key => {
+        const toolIdx = this.listData[idx].testing_tools.findIndex(item => item.name === key)
+        if (toolIdx < 0) return
+        this.listData[idx].testing_tools[toolIdx].enable = enable
+      })
+    },
+    selectServices(enable, branch) {
+      if (!enable) return
+      const idx = this.listData.findIndex(item => item.branch === branch)
+      this.services.forEach(key => {
+        const toolIdx = this.listData[idx].testing_tools.findIndex(item => item.name === key)
+        if (toolIdx < 0) return
+        this.listData[idx].testing_tools[toolIdx].enable = enable
+      })
+    },
+    handleReset() {
+      this.testingToolNames = []
+      this.formChange = false
+      this.fetchPipelineBranch()
     }
   }
 }
