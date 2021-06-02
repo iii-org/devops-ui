ad4a44ddf9c1ce604e8ec5e39540e32b8f7d6dc1
Asher Lin <thegaze77@gmail.com>
2021-06-02 18:14:41 +0800

    fix: filter wie report data

--- src/views/Scan/WIEReportViewer.vue
+++ src/views/Scan/WIEReportViewer.vue
@@ -1,21 +1,13 @@
 <template>
   <div v-loading="isLoading" :element-loading-text="$t('Loading')" class="app-container">
     <div v-if="!isLoading" class="d-flex justify-between align-center mb-2">
-      <div>
-        <span class="mr-3">Version：{{ jsonData.version }}</span>
-        <span>AppVersion：{{ jsonData.appVersion }}</span>
-      </div>
-      <el-button @click="handleDownload">下載報告（.xml）</el-button>
+      <el-button icon="el-icon-download" @click="handleDownload">下載報告 (.xml)</el-button>
     </div>
     <el-row>
       <el-col :span="24">
-        <el-collapse v-model="activeName" accordion>
-          <template v-for="(session, sessionIdx) in jsonData.Session">
-            <el-collapse-item
-              :key="session.requestId"
-              :title="`Session ${sessionIdx + 1}`"
-              :name="`Session ${sessionIdx + 1}`"
-            >
+        <el-tabs v-model="activeName">
+          <template v-for="(session, sessionIdx) in jsonData">
+            <el-tab-pane :key="session.requestId" :label="`${sessionIdx + 1}`" :name="`Session ${sessionIdx + 1}`">
               <template slot="title">
                 <div>
                   <span class="text-h6 mx-3">Session {{ sessionIdx + 1 }}</span>
@@ -23,7 +15,7 @@
                 </div>
               </template>
               <div class="p-5">
-                <el-row>
+                <!-- <el-row>
                   <el-col :span="12"><span class="font-weight-bold">URL：</span>{{ session.URL }}</el-col>
                   <el-col :span="12"><span class="font-weight-bold">Scheme：</span>{{ session.Scheme }}</el-col>
                   <el-col :span="12"><span class="font-weight-bold">Host：</span>{{ session.Host }}</el-col>
@@ -32,59 +24,67 @@
                     <span :span="12" class="font-weight-bold">AttackParamDescriptor：</span>
                     {{ session.AttackParamDescriptor || '-' }}
                   </el-col>
-                </el-row>
-                <el-divider />
+                </el-row> -->
 
-                <div class="text-h5 font-weight-bold">Issues</div>
+                <!-- Issues -->
+                <!-- <div class="text-h5 font-weight-bold">Issues</div> -->
                 <div v-for="issue in session.Issues" :key="issue.id">
-                  <div class="text-subtitle-2"># {{ issue.id }}</div>
-                  <div><span class="text-subtitle-1 font-weight-bold">CheckTypeID：</span>{{ issue.CheckTypeID }}</div>
-                  <div><span class="text-subtitle-1 font-weight-bold">EngineType：</span>{{ issue.EngineType }}</div>
+                  <div class="bg-gray-200 p-2">
+                    <span class="text-h5 font-medium mr-2">{{ issue.Name }}</span>
+                    <el-tag :type="mapSeverity('type', issue.Severity)" size="small">{{
+                      mapSeverity('name', issue.Severity)
+                    }}</el-tag>
+                  </div>
+
+                  <!-- <div class="text-subtitle-2"># {{ issue.id }}</div> -->
+                  <!-- <div><span class="text-subtitle-1 font-weight-bold">CheckTypeID：</span>{{ issue.CheckTypeID }}</div> -->
+                  <!-- <div><span class="text-subtitle-1 font-weight-bold">EngineType：</span>{{ issue.EngineType }}</div>
                   <div>
-                    <span class="text-subtitle-1 font-weight-bold">AttackHTTPRequest：</span
-                    >{{ issue.AttackHTTPRequest }}
+                    <span class="text-subtitle-1 font-weight-bold">AttackHTTPRequest：</span>
+                    {{ issue.AttackHTTPRequest }}
                   </div>
                   <div>
                     <span class="text-subtitle-1 font-weight-bold">VulnerabilityID：</span>{{ issue.VulnerabilityID }}
-                  </div>
-                  <div><span class="text-subtitle-1 font-weight-bold">Severity：</span>{{ issue.Severity }}</div>
-                  <div><span class="text-subtitle-1 font-weight-bold">Name：</span>{{ issue.Name }}</div>
+                  </div> -->
 
-                  <div>
-                    <div class="text-subtitle-1 font-weight-bold">Classification</div>
-                    <div
-                      v-for="(classification, clIdx) in issue.Classifications.Classification"
-                      :key="clIdx"
-                      class="mb-2"
-                    >
-                      <div class="font-weight-bold">{{ classification._ }}</div>
-                      <div>kind：{{ classification.kind }}</div>
-                      <div>identifier：{{ classification.identifier }}</div>
-                      <div>href：{{ classification.href }}</div>
-                    </div>
-
-                    <div>
-                      <div class="text-subtitle-1 font-weight-bold">ReportSection</div>
-                      <div v-for="ReportSection in issue.ReportSection" :key="ReportSection.Name" class="mb-2">
-                        <div class="text-subtitle-2 font-weight-bold">{{ ReportSection.Name }}</div>
-                        <div v-html="ReportSection.SectionText" />
-                      </div>
-                    </div>
+                  <!-- ReportSection -->
+                  <div v-for="ReportSection in issue.ReportSection" :key="ReportSection.Name" class="mb-2">
+                    <div class="text-subtitle-1 font-weight-bold text-blue-600">{{ ReportSection.Name }}</div>
+                    <div v-html="ReportSection.SectionText" />
                   </div>
+
+                  <!-- Classification -->
+                  <div class="text-subtitle-1 font-weight-bold text-blue-600">Classification</div>
+                  <div
+                    v-for="(classification, clIdx) in issue.Classifications.Classification"
+                    :key="clIdx"
+                    class="mb-2"
+                  >
+                    <div class="text-subtitle-2 font-weight-bold">{{ classification._ }}</div>
+                    <el-link type="primary" :href="classification.href" target="blank">{{
+                      classification.href
+                    }}</el-link>
+                    <!-- <div>kind：{{ classification.kind }}</div> -->
+                    <!-- <div>identifier：{{ classification.identifier }}</div> -->
+                  </div>
+
+                  <el-divider />
                 </div>
-                <el-divider />
 
-                <div class="text-h5 font-weight-bold">RawRequest</div>
-                <div class="text-subtitle-2"># {{ session.RawRequest.id }}</div>
-                <span class="font-weight-bold">RawRequest Content：</span>
-                <pre>{{ session.RawRequest._ }}</pre>
-                <el-divider />
-
-                <div class="text-h5 font-weight-bold">RawResponse</div>
-                <pre v-text="session.RawResponse" />
-                <el-divider />
+                <!-- RawRequest -->
 
                 <div class="text-h5 font-weight-bold">Request</div>
+                <!-- <div class="text-subtitle-2"># {{ session.RawRequest.id }}</div> -->
+                <pre>{{ formatRawData(session.RawRequest._) }}</pre>
+                <el-divider />
+
+                <!-- RawResponse -->
+                <div class="text-h5 font-weight-bold">Response</div>
+                <pre>{{ formatRawData(session.RawResponse) }}</pre>
+                <el-divider />
+
+                <!-- Request -->
+                <!-- <div class="text-h5 font-weight-bold">Request</div>
                 <ul>
                   <template v-for="(requestVal, requestKey) in session.Request">
                     <li v-if="requestKey === 'Headers'" :key="requestKey">
@@ -109,9 +109,10 @@
                     </li>
                   </template>
                 </ul>
-                <el-divider />
+                <el-divider /> -->
 
-                <div class="text-h5 font-weight-bold">Response</div>
+                <!-- Response -->
+                <!-- <div class="text-h5 font-weight-bold">Response</div>
                 <ul>
                   <template v-for="(responseVal, responseKey) in session.Response">
                     <li v-if="responseKey === 'Headers'" :key="responseKey">
@@ -132,11 +133,11 @@
                       {{ responseVal }}
                     </li>
                   </template>
-                </ul>
+                </ul> -->
               </div>
-            </el-collapse-item>
+            </el-tab-pane>
           </template>
-        </el-collapse>
+        </el-tabs>
       </el-col>
     </el-row>
   </div>
@@ -181,11 +182,11 @@ export default {
         })
     },
     formatter(data) {
-      const result = data.Sessions
-      result.Session.forEach(session => {
+      const sessions = data.Sessions.Session.filter(session => this.formatIssues(session.Issues).length > 0)
+      sessions.forEach(session => {
         session.Issues = this.formatIssues(session.Issues)
       })
-      return result
+      return sessions
     },
     formatIssues(rowIssues) {
       const type = typeof rowIssues
@@ -202,6 +203,19 @@ export default {
       link.href = URL.createObjectURL(blob)
       link.click()
       URL.revokeObjectURL(blob)
+    },
+    formatRawData(data) {
+      return data
+    },
+    mapSeverity(key, status) {
+      const mapping = {
+        0: { type: 'info', name: 'Info' },
+        1: { type: 'primary', name: 'Low' },
+        2: { type: 'warning', name: 'Medium' },
+        3: { type: 'danger', name: 'High' },
+        4: { type: 'danger', name: 'Critical' }
+      }
+      return mapping[status][key]
     }
   }
 }
