c98e917bacacb4b5f7657579f9d0d7733088e455
yuan <13202145+becory@users.noreply.github.com>
2021-06-24 16:21:10 +0800

    fix: issue board quick panel z-index

--- src/views/Project/IssueBoards/components/RightPanel.vue
+++ src/views/Project/IssueBoards/components/RightPanel.vue
@@ -99,7 +99,7 @@ export default {
   transition: all .25s cubic-bezier(.7, .3, .1, 1);
   transform: translate(100%);
   background: #fff;
-  z-index: 2045;
+  z-index: 1000;
 }
 .show {
   transition: all .3s cubic-bezier(.7, .3, .1, 1);
