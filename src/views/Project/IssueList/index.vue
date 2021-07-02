3db13302114d3c364207789a278853e7dc87a91c
yuan <13202145+becory@users.noreply.github.com>
2021-07-02 09:10:39 +0800

    fix: issue list right click overflow

--- src/views/Project/IssueList/index.vue
+++ src/views/Project/IssueList/index.vue
@@ -613,28 +613,34 @@ export default {
         })
     },
     handleContextMenu(row, column, event) {
-      this.$refs.contextmenu.$refs.contextmenu.show()
+      event.preventDefault()
       const eventX = event.pageX
       const eventY = event.pageY
-      const contextmenuPosition = {
-        top: eventY,
-        left: eventX
-      }
-      const contextmenuWidth = this.$refs.contextmenu.$refs.contextmenu.$el.clientWidth
-      const contextmenuHeight = this.$refs.contextmenu.$refs.contextmenu.$el.clientHeight
-      if (contextmenuHeight + eventY >= window.innerHeight) {
-        contextmenuPosition.top -= contextmenuHeight
-      }
-      if (contextmenuWidth + eventX >= window.innerWidth) {
-        contextmenuPosition.left -= contextmenuWidth
-      }
-      this.contextMenu.top = contextmenuPosition.top
-      this.contextMenu.left = contextmenuPosition.left
-      this.contextMenu.row = row
-      this.contextMenu.visible = true
-      this.$refs.contextmenu.$refs.contextmenu.show({ top: this.contextMenu.top, left: this.contextMenu.left })
-      event.preventDefault()
-      document.addEventListener('click', this.hideContextMenu)
+      this.$refs.contextmenu.$refs.contextmenu.show()
+      this.$nextTick(() => {
+        const contextmenuPosition = {
+          top: eventY,
+          left: eventX
+        }
+        const contextmenuWidth = this.$refs.contextmenu.$refs.contextmenu.$el.clientWidth
+        const contextmenuHeight = this.$refs.contextmenu.$refs.contextmenu.$el.clientHeight
+        if (contextmenuWidth <= 50 && contextmenuWidth <= 50) {
+          console.log(contextmenuWidth, contextmenuHeight)
+          this.handleContextMenu(row, column, event)
+        }
+        if (contextmenuHeight + eventY >= window.innerHeight) {
+          contextmenuPosition.top -= contextmenuHeight
+        }
+        if (contextmenuWidth + eventX >= window.innerWidth) {
+          contextmenuPosition.left -= contextmenuWidth
+        }
+        this.contextMenu.top = contextmenuPosition.top
+        this.contextMenu.left = contextmenuPosition.left
+        this.contextMenu.row = row
+        this.contextMenu.visible = true
+        this.$refs.contextmenu.$refs.contextmenu.style = { top: this.contextMenu.top + 'px', left: this.contextMenu.left + 'px' }
+        document.addEventListener('click', this.hideContextMenu)
+      })
     },
     hideContextMenu() {
       this.contextMenu.visible = false
