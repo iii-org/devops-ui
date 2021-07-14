6a46797f910de52a9d47adb96e473e805967239d
Clay <noitseuq.rm@gmail.com>
2021-07-14 11:32:53 +0800

    chore: modify ParticipateProject layout

--- src/views/SystemSettings/AccountManage/components/ParticipateProject.vue
+++ src/views/SystemSettings/AccountManage/components/ParticipateProject.vue
@@ -1,68 +1,80 @@
 <template>
-  <el-row class="app-container">
-    <el-col class="text-xl mr-3">
+  <div class="app-container">
+    <div class="text-xl mr-3">
       <el-button type="text" size="medium" icon="el-icon-arrow-left" class="previous" @click="handleBackPage">
         {{ $t('general.Back') }}
       </el-button>
-      <el-divider />
-      <el-table
-        v-loading="listLoading"
-        :data="pagedData"
-        :element-loading-text="$t('Loading')"
-        border
-        height="70vh"
-        fit
-        highlight-current-row
-      >
-        <!-- <el-table-column align="center" prop="name" :label="$t('general.ProjectId')" width="250" /> -->
-        <el-table-column align="center" prop="display" :label="$t('Project.Name')" width="430" />
-        <el-table-column align="center" prop="owner_name" :label="$t('Project.Owner')" width="200" />
-        <el-table-column align="center" prop="start_date" :label="$t('Project.StartDate')" width="120" />
-        <el-table-column align="center" prop="due_date" :label="$t('Project.DueDate')" width="120" />
-        <el-table-column align="center" :label="$t('general.Actions')" width="140">
-          <template slot-scope="scope">
-            <el-popconfirm
-              :confirm-button-text="$t('general.Remove')"
-              :cancel-button-text="$t('general.Cancel')"
-              icon="el-icon-info"
-              icon-color="red"
-              :title="$t('Member.confirmRemove')"
-              @onConfirm="handleDelete(scope.row.id)"
+      <span class="ml-2 text-xl">
+        <span>{{ $t('Issue.FilterDimensions.assigned_to') }}</span>:
+        <span>{{ projectMember }}</span>
+      </span>
+    </div>
+    <el-divider />
+    <el-table
+      v-loading="listLoading"
+      :data="pagedData"
+      :element-loading-text="$t('Loading')"
+      border
+      height="100%"
+      :cell-style="{ height: rowHeight + 'px' }"
+      fit
+      highlight-current-row
+    >
+      <!-- <el-table-column align="center" prop="name" :label="$t('general.ProjectId')" width="250" /> -->
+      <el-table-column align="center" prop="display" :label="$t('Project.Name')" width="500" />
+      <el-table-column align="center" prop="owner_name" :label="$t('Project.Owner')" />
+      <el-table-column align="center" prop="start_date" :label="$t('Project.StartDate')" />
+      <el-table-column align="center" prop="due_date" :label="$t('Project.DueDate')" />
+      <el-table-column align="center" :label="$t('general.Actions')">
+        <template slot-scope="scope">
+          <el-popconfirm
+            :confirm-button-text="$t('general.Remove')"
+            :cancel-button-text="$t('general.Cancel')"
+            icon="el-icon-info"
+            icon-color="red"
+            :title="$t('Member.confirmRemove')"
+            @onConfirm="handleDelete(scope.row.id)"
+          >
+            <el-button
+              slot="reference"
+              size="mini"
+              type="danger"
+              icon="el-icon-delete"
+              :disabled="isDisabled"
             >
-              <el-button
-                slot="reference"
-                size="mini"
-                type="danger"
-                icon="el-icon-delete"
-                :disabled="isDisabled"
-              >
-                {{ $t('general.Remove') }}
-              </el-button>
-            </el-popconfirm>
-          </template>
-        </el-table-column>
-      </el-table>
-      <pagination
-        v-if="listData && listData.length > 0"
-        :total="filteredData.length"
-        :page="listQuery.page"
-        :limit="listQuery.limit"
-        :page-sizes="[listQuery.limit]"
-        :layout="'total, prev, pager, next'"
-        @pagination="onPagination"
-      />
-    </el-col>
-  </el-row>
+              {{ $t('general.Remove') }}
+            </el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination
+      v-if="listData && listData.length > 0"
+      :total="filteredData.length"
+      :page="listQuery.page"
+      :limit="listQuery.limit"
+      :page-sizes="[listQuery.limit]"
+      :layout="'total, prev, pager, next'"
+      @pagination="onPagination"
+    />
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import { getParticipateProject, deleteProjectMember } from '@/api/projects'
 import MixinElTableWithAProject from '@/mixins/MixinElTableWithAProject'
+import { getUserInfo } from '@/api/user'
 
 export default {
   name: 'ParticipateProject',
   mixins: [MixinElTableWithAProject],
+  data() {
+    return {
+      rowHeight: 70,
+      projectMember: ''
+    }
+  },
   computed: {
     ...mapGetters(['userId']),
     isDisabled() {
@@ -73,6 +85,7 @@ export default {
     '$route.params.user_id': {
       handler(val) {
         this.fetchData(val)
+        this.getUserInfo(val)
       },
       immediate: true
     }
@@ -84,6 +97,10 @@ export default {
       const projectsByUser = await getParticipateProject(user_id)
       this.listData = projectsByUser.data
       this.listLoading = false
+    },
+    async getUserInfo(user_id) {
+      const userInfo = await getUserInfo(user_id)
+      this.projectMember = userInfo.name
     },
     onPagination(listQuery) {
       this.listQuery = listQuery
