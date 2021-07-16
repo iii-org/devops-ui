d2afc5ae6093b33bf97eb977dcdc8d9c0f931d70
yuan <13202145+becory@users.noreply.github.com>
2021-07-16 18:09:15 +0800

    fix: tailwind color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,6 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    minWidth: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%'
-    },
     extend: {
       minWidth: {
         '300': '300px'
