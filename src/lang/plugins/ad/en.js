b32cf8a29f2cec3663c6479733e20a2f2eff1d04
wyuchi99 <wyuchi99@gmail.com>
2021-07-20 13:57:51 +0800

    add ad plugin descripiton

--- src/lang/plugins/ad/en.js
+++ src/lang/plugins/ad/en.js
@@ -1,5 +1,11 @@
 export default {
-  'description': 'description',
-  'argument key1': { title: 'title', placeholder: 'placeholder' },
-  'argument key2': { title: 'title', placeholder: 'placeholder' }
+  'description': 'Description',
+  'host': { title: 'AD host', placeholder: 'localhost:port' },
+  'ou': { title: 'AD OU ', placeholder: 'please enter sync organization unit' },
+  'domain': { title: 'AD base DN', placeholder: 'cn=admin,dc=example.com' },
+  'account': { title: 'account', placeholder: 'please enter connect ad account' },
+  'password': { title: 'password', placeholder: 'please enter connect ad account password' },
+  'default_password': { title: 'System Default Password', placeholder: 'password' },
+  'connect_timeout': { title: 'AD Connect time', placeholder: '5s' },
+  'default_role_id': { title: 'Default User Role', placeholder: '3' }
 }
