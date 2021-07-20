5ee462469071a47db34daec0c634075da72be94b
wyuchi99 <wyuchi99@gmail.com>
2021-07-20 14:28:39 +0800

    modify ad plugin i18n  language

--- src/lang/plugins/ad/en.js
+++ src/lang/plugins/ad/en.js
@@ -1,11 +1,11 @@
 export default {
   'description': 'Description',
-  'host': { title: 'AD host', placeholder: 'localhost:port' },
-  'ou': { title: 'AD OU ', placeholder: 'please enter sync organization unit' },
-  'domain': { title: 'AD base DN', placeholder: 'cn=admin,dc=example.com' },
-  'account': { title: 'account', placeholder: 'please enter connect ad account' },
-  'password': { title: 'password', placeholder: 'please enter connect ad account password' },
-  'default_password': { title: 'System Default Password', placeholder: 'password' },
-  'connect_timeout': { title: 'AD Connect time', placeholder: '5s' },
-  'default_role_id': { title: 'Default User Role', placeholder: '3' }
+  'host': { title: 'AD host', hint: 'localhost:port', placeholder: '' },
+  'ou': { title: 'AD OU ', hint: 'please enter sync organization unit', placeholder: '' },
+  'domain': { title: 'AD base DN', hint: 'cn=admin,dc=example.com', placeholder: '' },
+  'account': { title: 'account', hint: 'please enter connect ad account', placeholder: 'account' },
+  'password': { title: 'password', hint: 'please enter connect ad account password', placeholder: 'password' },
+  'default_password': { title: 'System Default Password', hint: 'password', placeholder: 'password' },
+  'connect_timeout': { title: 'AD Connect time', hint: 'connection time', placeholder: '5s' },
+  'default_role_id': { title: 'Default User Role', hint: 'default user role in system', placeholder: '3' }
 }
--- src/lang/plugins/ad/zh-TW.js
+++ src/lang/plugins/ad/zh-TW.js
@@ -1,11 +1,11 @@
 export default {
   'description': '描述',
-  'host': { title: '主機位址', placeholder: '請輸入 AD 伺服器位址' },
-  'ou': { title: 'OU', placeholder: '請輸入 OU' },
-  'domain': { title: 'Domain', placeholder: '請輸入 Domain' },
-  'account': { title: '帳號', placeholder: '請輸入 AD 管理者帳號' },
-  'password': { title: '密碼', placeholder: '請輸入 AD 管理者密碼' },
-  'default_password': { title: '預設密碼', placeholder: '請輸入預設密碼' },
-  'connect_timeout': { title: 'Connect timeout', placeholder: '請輸入 Connect timeout' },
-  'default_role_id': { title: '預設角色 ID', placeholder: '請輸入預設角色 ID' }
+  'host': { title: '主機位址', hint: '請輸入 AD 伺服器位址', placeholder: '127.0.0.1:389' },
+  'ou': { title: '組織', hint: '請輸入 OU', placeholder: '部門名稱' },
+  'domain': { title: '網域', hint: '請輸入 base dn', placeholder: 'test-ad.corp' },
+  'account': { title: '帳號', hint: '請輸入 AD 連線帳號', placeholder: 'account' },
+  'password': { title: '密碼', hint: '請輸入 AD 連線密碼', placeholder: 'password' },
+  'default_password': { title: '新建帳號預設密碼', hint: '請輸入預設密碼', placeholder: 'password' },
+  'connect_timeout': { title: '與AD連線時間', hint: '請輸入 Connect timeout', placeholder: '5' },
+  'default_role_id': { title: '預設角色 ID', hint: '請輸入預設角色 ID', placeholder: '3' }
 }
