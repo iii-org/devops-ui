3d5f4ed682ead785289fb96ca687cf4c8e0d10a4
Asher Lin <thegaze77@gmail.com>
2021-07-20 08:40:14 +0800

    chore: modify wie report layout

--- src/views/Scan/WIEReportViewer.vue
+++ src/views/Scan/WIEReportViewer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div id="wie-report-header" v-loading="isLoading" :element-loading-text="$t('Loading')" class="px-5 h-full overflow-y-auto overflow-x-hidden">
+  <div
+    id="wie-report-header" v-loading="isLoading" :element-loading-text="$t('Loading')"
+    class="px-5 h-full overflow-x-hidden overflow-y-auto"
+  >
+    <div class="drawer w-1/4" :class="isShowDrawer ? '' : 'drawer-close'">
+      <span class="button" @click="showDrawer">
+        <el-icon :class="isShowDrawer ? 'el-icon-close' : 'el-icon-tickets'" />
+      </span>
+      <div class="ml-2 overflow-y-auto h-full">
+        <div class="text-title mb-3">In this report</div>
+        <div v-for="issue in groupByIssueSeverity" :key="issue.id" class="cursor-pointer py-1">
+          <span class="badge" :class="mapSeverity('bgColor', issue.severity)">{{ mapSeverity('name', issue.severity) }}</span>
+          <div class="hover:underline py-1 leading-snug" @click="toIssue(issue.id)"> {{ issue.name }}</div>
+        </div>
+      </div>
+    </div>
     <el-backtop target="#wie-report-header" />
     <div v-if="!isLoading" class="flex justify-between items-center my-5">
       <h3>「{{ selectedProject.display }}」{{ $t('WebInspect.TestReport') }}</h3>
@@ -8,18 +23,16 @@
         {{ $t('WebInspect.DownloadReport') }} (.xml)
       </el-button>
     </div>
-
     <el-row :gutters="12">
-      <el-col v-for="issue in groupByIssueSeverity" :key="issue.id" class="mb-8" :span="24">
+      <el-col v-for="issue in groupByIssueSeverity" :ref="issue.id" :key="issue.id" class="mb-8" :span="24">
         <div class="sticky top-0">
-          <div class="bg-blue-500 text-white p-3 font-small font-semibold" :type="mapSeverity('type', issue.severity)">
+          <div class="text-white p-3 font-small font-semibold" :class="mapSeverity('bgColor', issue.severity)">
             {{ mapSeverity('name', issue.severity) + ' Issue' }}
           </div>
-          <div class="p-3 bg-gray-200  text-xl font-medium z-0">
+          <div class="p-3 bg-gray-200 text-xl font-medium z-0">
             {{ issue.name }}
           </div>
         </div>
-
         <div class="p-6 bg-white shadow rounded">
           <div class="text-xl font-semibold">Request：</div>
           <pre class="w-full whitespace-pre-wrap text-sm break-words">{{ issue.request }}</pre>
@@ -50,20 +63,20 @@
 </template>
 
 <script>
-import { getWebInspectReport } from '@/api/webInspect'
 import xml2js from 'xml2js'
 import { mapGetters } from 'vuex'
+import { getWebInspectReport } from '@/api/webInspect'
 const parser = new xml2js.Parser({ explicitArray: false, mergeAttrs: true })
 
 export default {
   name: 'WIEViewer',
   data() {
     return {
-      activeName: '',
       runAt: null,
       isLoading: false,
       xmlData: '',
-      jsonData: []
+      jsonData: [],
+      isShowDrawer: false
     }
   },
   computed: {
@@ -80,7 +93,7 @@ export default {
           classifications: issue.Classifications.Classification
         }))
       )
-      return result.sort((a, b) => a.severity - b.severity)
+      return result.sort((a, b) => b.severity - a.severity)
     }
   },
   mounted() {
@@ -100,7 +113,6 @@ export default {
         .parseStringPromise(this.xmlData)
         .then(result => {
           this.jsonData = this.formatter(result)
-          this.activeName = 'Session 1'
           this.isLoading = false
         })
         .catch(function(err) {
@@ -133,11 +145,11 @@ export default {
     },
     mapSeverity(key, status) {
       const mapping = {
-        0: { type: 'info', name: 'Info' },
-        1: { type: 'primary', name: 'Low' },
-        2: { type: 'warning', name: 'Medium' },
-        3: { type: 'danger', name: 'High' },
-        4: { type: 'danger', name: 'Critical' }
+        0: { name: 'Info', bgColor: 'bg-green-500' },
+        1: { name: 'Low', bgColor: 'bg-blue-500' },
+        2: { name: 'Medium', bgColor: 'bg-yellow-500' },
+        3: { name: 'High', bgColor: 'bg-pink-500' },
+        4: { name: 'Critical', bgColor: 'bg-red-600' }
       }
       return mapping[status][key]
     },
@@ -148,7 +160,29 @@ export default {
         .local()
         .format('YYYY-MM-DD HH:mm:ss')
       return localTime
+    },
+    toIssue(target) {
+      this.$refs[target][0].$el.scrollIntoView({ behavior: 'smooth' })
+    },
+    showDrawer() {
+      this.isShowDrawer = !this.isShowDrawer
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.button {
+  @apply bg-green-500 text-white pl-2 pr-5 shadow py-2 rounded relative -left-12 top-52;
+  transform: translate3d(-500px, 0, 0);
+}
+.drawer {
+  @apply fixed h-full right-0 bg-white shadow-lg z-50 transition duration-500 p-5;
+}
+.drawer-close {
+  @apply translate-x-full;
+}
+.badge{
+  @apply uppercase text-white text-xs tracking-wide font-semibold px-2 py-1 rounded-sm cursor-default
+}
+</style>
