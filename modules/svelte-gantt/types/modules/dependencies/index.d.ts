17ab458eaaca7537597201ef6d52e61af82fd7c7
yuan <13202145+becory@users.noreply.github.com>
2021-07-27 14:40:31 +0800

    chore: install gantt

--- modules/svelte-gantt/types/modules/dependencies/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentCreator, Component } from '../../core/svelte';
-import { DependencyModel } from './dependency';
-declare var SvelteGanttDependencies: ComponentCreator<Component<Record<string, any>>, Record<string, any>>;
-export { SvelteGanttDependencies, DependencyModel };
