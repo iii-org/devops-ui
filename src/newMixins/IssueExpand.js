07638152a7aa15b78ec6efffa6200122abd1fdf1
yuan <13202145+becory@users.noreply.github.com>
2021-08-07 00:31:48 +0800

    fix: issue list ExpandSection hotfix

--- src/newMixins/IssueExpand.js
+++ src/newMixins/IssueExpand.js
@@ -1,6 +1,7 @@
+import { deleteIssueRelation, updateIssue } from '@/api/issue'
 
 export default {
-  name: 'IssueExpand',
+  name: 'ExpandSection',
   props: {
     issue: {
       type: Object,
@@ -8,6 +9,38 @@ export default {
     }
   },
   methods: {
-
+    async removeIssueRelation(childIssueId) {
+      this.listLoading = true
+      try {
+        await updateIssue(childIssueId, { parent_id: '' })
+        this.$message({
+          title: this.$t('general.Success'),
+          message: this.$t('Notify.Updated'),
+          type: 'success'
+        })
+        await this.loadData()
+      } catch (err) {
+        console.error(err)
+      }
+      this.listLoading = false
+    },
+    async removeRelationIssue(relation_id) {
+      this.listLoading = true
+      try {
+        await deleteIssueRelation(relation_id)
+        this.$message({
+          title: this.$t('general.Success'),
+          message: this.$t('Notify.Updated'),
+          type: 'success'
+        })
+        await this.loadData()
+      } catch (err) {
+        console.error(err)
+      }
+      this.listLoading = false
+    },
+    handleEdit(issueId) {
+      this.$router.push({ name: 'issue-detail', params: { issueId }})
+    }
   }
 }
