5bd17b0cbe227f66789d4f23ed282d9fde2562aa
Clay <noitseuq.rm@gmail.com>
2021-09-27 16:36:52 +0800

    chore: add new api

--- src/api/commitList.js
+++ src/api/commitList.js
@@ -1,3 +1,4 @@
 import request from '@/utils/request'
 
 export const getCommitListByBranch = (rId, params) => request.get('repositories/' + rId + '/commits', { params })
+export const getMemberCommitListByBranch = (rId, params) => request.get(`repositories/${rId}/members_commits`, { params })
