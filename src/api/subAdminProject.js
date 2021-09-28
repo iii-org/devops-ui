fb72ade1c4f6c914f22cf427954bb61c5b359381
yuan <13202145+becory@users.noreply.github.com>
2021-09-28 14:23:03 +0800

    fix: Admin QA setting data error

--- src/api/subAdminProject.js
+++ src/api/subAdminProject.js
@@ -4,4 +4,4 @@ export const getAdminProjects = () => request.get(`/project_permission/admin_pro
 export const getSubAdminProjects = (params) => request.get(`/project_permission/subadmin_projects`, { params })
 export const getSubAdmins = () => request.get(`/project_permission/subadmins`)
 export const setProjectPermission = (data) => request.post(`/project_permission/set_permission`, data)
-export const deleteProjectPermission = (data) => request.delete(`/project_permission/set_permission`, data)
+export const deleteProjectPermission = (data) => request.delete(`/project_permission/set_permission`, { data: data })
