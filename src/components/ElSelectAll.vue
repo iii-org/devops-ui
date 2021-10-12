56bce076cfa8a672c15d57b615d3bc4921159fbc
yuan <13202145+becory@users.noreply.github.com>
2021-10-12 09:46:40 +0800

    chore: delete remark

--- src/components/ElSelectAll.vue
+++ src/components/ElSelectAll.vue
@@ -98,28 +98,20 @@ export default {
     onChange(val) {
       // eslint-disable-next-line no-debugger
       const allValues = []
-      // 保留所有值
       for (const item of this.mdoptionsList) {
         allValues.push(item.value)
       }
-      // 用来储存上一次的值，可以进行对比
       const oldVal = this.oldMdoptionsValue.length === 1 ? [] : this.oldMdoptionsValue[1] || []
-      // 若是全部选择
       if (val.includes('all')) this.selected = allValues
-      // 取消全部选中  上次有 当前没有 表示取消全选
       if (oldVal.includes('all') && !val.includes('all')) this.selected = []
-      // 点击非全部选中  需要排除全部选中 以及 当前点击的选项
-      // 新老数据都有全部选中
       if (oldVal.includes('all') && val.includes('all')) {
         const index = val.indexOf('all')
-        val.splice(index, 1) // 排除全选选项
+        val.splice(index, 1)
         this.selected = val
       }
-      // 全选未选 但是其他选项全部选上 则全选选上 上次和当前 都没有全选
       if (!oldVal.includes('all') && !val.includes('all')) {
         if (val.length === allValues.length - 1) this.selected = ['all'].concat(val)
       }
-      // 储存当前最后的结果 作为下次的老数据
       this.oldMdoptionsValue[1] = this.selected
     }
   }
