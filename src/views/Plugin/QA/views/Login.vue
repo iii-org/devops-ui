98a6abc8faff140c3c3af259dda7d2d7c090db4a
yuan <13202145+becory@users.noreply.github.com>
2021-10-12 09:59:04 +0800

    chore: delete remark

--- src/views/Plugin/QA/views/Login.vue
+++ src/views/Plugin/QA/views/Login.vue
@@ -137,9 +137,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
 $bg: #283443;
 $light_gray: #fff;
 $cursor: #fff;
@@ -154,7 +151,7 @@ $cursor: #fff;
 .login-container {
   min-height: 100%;
   width: 100%;
-  background-image: url('~@/assets/login-bg.png');
+  //background-image: url('~@/assets/login-bg.png');
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-position: center;
