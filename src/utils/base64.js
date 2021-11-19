590e20647c3cc7cebcae505e698348d58ce291fb
Clay <noitseuq.rm@gmail.com>
2021-11-19 17:50:01 +0800

    fix: btoa error

--- src/utils/base64.js
+++ src/utils/base64.js
@@ -1,3 +1,10 @@
+const a = 'base64'
+const b = 'binary'
+
 export function btoa(code) {
-  return Buffer.from(code).toString('base64')
+  return Buffer.from(code, b).toString(a)
+}
+
+export function atob(code) {
+  return Buffer.from(code, a).toString(b)
 }
