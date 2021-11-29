c0c8a8ff8331eb5f22bcd47a004772f86c6e5abb
W.H. Lin <thegaze77@gmail.com>
2021-11-29 09:43:51 +0800

    feat: adjust project users title

--- src/views/Plan/Overview/components/ProjectUsersCard.vue
+++ src/views/Plan/Overview/components/ProjectUsersCard.vue
@@ -1,18 +1,43 @@
 <template>
-  <el-card class="mb-3" shadow="never">
+  <el-card
+    class="mb-3"
+    shadow="never"
+  >
     <div class="flex items-center font-semibold h-8">
       <em class="el-icon-user mx-1" />
       {{ $t('Dashboard.ProjectUsers') }}
     </div>
-    <el-empty v-if="userList.length === 0" :description="$t('general.NoData')" :image-size="100" />
-    <el-table v-else :data="userList" style="width: 100%" stripe>
-      <el-table-column prop="role_name" :label="$t('Project.Title')" />
-      <el-table-column prop="name" :label="$t('general.Name')">
+    <el-empty
+      v-if="userList.length === 0"
+      :description="$t('general.NoData')"
+      :image-size="100"
+    />
+    <el-table
+      v-else
+      :data="userList"
+      style="width: 100%"
+      stripe
+    >
+      <el-table-column
+        prop="role_name"
+        :label="$t('Project.Title')"
+      >
+        <template slot-scope="scope">
+          {{ getRoleName(scope.row.id) }}
+        </template>
+      </el-table-column>>
+      <el-table-column
+        prop="name"
+        :label="$t('general.Name')"
+      >
         <template slot-scope="scope">
           {{ `${scope.row.name} (${scope.row.login})` }}
         </template>
       </el-table-column>
-      <el-table-column prop="email" :label="$t('general.Email')" />
+      <el-table-column
+        prop="email"
+        :label="$t('general.Email')"
+      />
     </el-table>
   </el-card>
 </template>
@@ -29,7 +54,13 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['selectedProjectId'])
+    ...mapGetters(['selectedProjectId', 'selectedProject'])
+  },
+  methods: {
+    getRoleName(id) {
+      const isProjectOwner = id === this.selectedProject.owner_id
+      return isProjectOwner ? this.$t('Member.ProjectOwner') : this.$t('Member.ProjectMember')
+    }
   }
 }
 </script>
