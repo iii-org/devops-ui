238f83c7b7d9294493a04d744bfcc06c358311fd
Webster Liang <caest85479@gmail.com>
2022-01-25 16:28:16 +0800

    fix: default latest version in overview (#3022)

--- src/views/Plan/Overview/index.vue
+++ src/views/Plan/Overview/index.vue
@@ -105,7 +105,7 @@ export default {
   },
   mounted() {
     this.fetchVersionList()
-    this.fetchAllData()
+    // this.fetchAllData()
   },
   methods: {
     ...mapActions('projects', ['getProjectUserList']),
@@ -137,15 +137,14 @@ export default {
       const hasVersion = res.data.versions.length > 0
       if (hasVersion) {
         this.versionList = res.data.versions
-        this.versionList.reduce((acc, cur) => {
-          if (cur.updated_on >= acc.updated_on && cur.status === 'open') {
-            this.selectedVersion = cur.id
-          } else if (acc.status === 'open') {
-            this.selectedVersion = acc.id
-            return acc
+        for (const item of this.versionList) {
+          const nowDate = Date.parse(new Date(Date.now()))
+          const dueDate = Date.parse(new Date(item.due_date))
+          if (dueDate >= nowDate && item.status === 'open') {
+            this.selectedVersion = item.id
+            break
           }
-          return cur
-        })
+        }
       } else {
         this.clearSelectedVersion()
         this.versionList = []
