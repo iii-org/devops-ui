c0e11e5cc7ad26a4dfc3dfce8c2be410e803c0d8
Chun jung Lin <corilin@gmail.com>
2022-06-09 13:37:39 +0800

    add excalidraw i18n

--- src/lang/plugins/excalidraw/en.js
+++ src/lang/plugins/excalidraw/en.js
@@ -2,8 +2,10 @@ export default {
   'description': 'Excalidraw collaboration whiteboard',
   'arguments': {
     'excalidraw-url': { title: 'Server URL', hint: 'please enter Server URL', placeholder: 'Server URL of the Excalidraw server.' },
+    'excalidraw-db-host': { title: 'DB Host', hint: 'please enter the db url', placeholder: 'DB URL' },
+    'excalidraw-db-database': { title: 'DB Name', hint: 'please enter the db name', placeholder: 'Database Name' },
+    'excalidraw-db-port': { title: 'DB Port', hint: 'please enter the db port', placeholder: 'DB Port' },
     'excalidraw-db-account': { title: 'DB User Name', hint: 'please enter the db user account', placeholder: 'DB User Account' },
-    'username': { title: 'Username', hint: 'please enter username', placeholder: 'Username of Checkmarx.' },
-    'password': { title: 'Password', hint: 'please enter password', placeholder: 'Password of Checkmarx.' }
+    'excalidraw-db-password': { title: 'DB User Password', hint: 'please enter the db user password', placeholder: 'DB User Password' }
   }
 }
--- src/lang/plugins/excalidraw/zh-TW.js
+++ src/lang/plugins/excalidraw/zh-TW.js
@@ -1,10 +1,11 @@
 export default {
-  'description': 'Checkmarx 原始碼掃描',
+  'description': 'Excalidraw 白板協作工具',
   'arguments': {
     'excalidraw-url': { title: '伺服器位置', hint: '請輸入伺服器位置', placeholder: 'Excalidraw 伺服器的網址.'},
-    'excalidraw-db-account': { title: 'DB User Name', hint: 'please enter the db user account', placeholder: 'DB User Account'},
-    'cm-url': { title: '伺服器位置', hint: '請輸入伺服器位置', placeholder: 'Checkmarx 伺服器的網址' },
-    'username': { title: '使用者名稱', hint: '請輸入使用者名稱', placeholder: 'Checkmarx 的使用者名稱' },
-    'password': { title: '密碼', hint: '請輸入密碼', placeholder: 'Checkmarx 的使用者密碼' }
+    'excalidraw-db-host': { title: '資料庫位址', hint: '請輸入資料庫連線位址', placeholder: '資料庫URL位址' },
+    'excalidraw-db-database': { title: '資料庫名稱', hint: '請輸入資料庫名稱', placeholder: '資料庫名稱(Database)' },
+    'excalidraw-db-port': { title: '資料庫埠號', hint: '請輸入資料庫連線埠號', placeholder: '資料庫埠號(Port)' },
+    'excalidraw-db-account': { title: '資料庫使用者名稱', hint: '請輸入資料庫使用者名稱', placeholder: '資料庫使用者名稱(User Name)' },
+    'excalidraw-db-password': { title: '資料庫使用者密碼', hint: '請輸入資料庫使用者密碼', placeholder: '資料庫使用者密碼(User password)'}
   }
 }
