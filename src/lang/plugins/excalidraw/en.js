7daad7b36cb9791bf1817e919d6d572fffe16d96
Chun jung Lin <corilin@gmail.com>
2022-06-09 09:16:21 +0800

    add excalidraw i18n

---
+++ src/lang/plugins/excalidraw/en.js
@@ -0,0 +1,9 @@
+export default {
+  'description': 'Excalidraw collaboration whiteboard',
+  'arguments': {
+    'excalidraw-url': { title: 'Server URL', hint: 'please enter Server URL', placeholder: 'Server URL of the Excalidraw server.' },
+    'excalidraw-db-account': { title: 'DB User Name', hint: 'please enter the db user account', placeholder: 'DB User Account' },
+    'username': { title: 'Username', hint: 'please enter username', placeholder: 'Username of Checkmarx.' },
+    'password': { title: 'Password', hint: 'please enter password', placeholder: 'Password of Checkmarx.' }
+  }
+}
---
+++ src/lang/plugins/excalidraw/zh-TW.js
@@ -0,0 +1,10 @@
+export default {
+  'description': 'Checkmarx 原始碼掃描',
+  'arguments': {
+    'excalidraw-url': { title: '伺服器位置', hint: '請輸入伺服器位置', placeholder: 'Excalidraw 伺服器的網址.'},
+    'excalidraw-db-account': { title: 'DB User Name', hint: 'please enter the db user account', placeholder: 'DB User Account'},
+    'cm-url': { title: '伺服器位置', hint: '請輸入伺服器位置', placeholder: 'Checkmarx 伺服器的網址' },
+    'username': { title: '使用者名稱', hint: '請輸入使用者名稱', placeholder: 'Checkmarx 的使用者名稱' },
+    'password': { title: '密碼', hint: '請輸入密碼', placeholder: 'Checkmarx 的使用者密碼' }
+  }
+}
