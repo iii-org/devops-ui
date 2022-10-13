dfcbf14bcf1dfe1df274e4e357186b7a5f237efd
Webster <caesr85479@gmail.com>
2022-10-13 17:43:38 +0800

    feat: excalidraw add restore feature (#4485)

--- src/api_v2/excalidraw.js
+++ src/api_v2/excalidraw.js
@@ -8,3 +8,6 @@ export const updateExcalidraw = (excalidraw_id, data) => request.patch(`/v2/exca
   data, { headers: { 'Content-Type': 'multipart/form-data' }}
 )
 export const deleteExcalidraw = (excalidraw_id) => request.delete(`/v2/excalidraw/${excalidraw_id}`)
+export const getExcalidrawHistory = (excalidraw_id) => request.get(`/v2/excalidraw/${excalidraw_id}/historys`)
+export const createExcalidrawHistory = (excalidraw_id) => request.post(`/v2/excalidraw/${excalidraw_id}/historys`)
+export const restoreExcalidrawHistory = (history_id) => request.put(`/v2/excalidraw/${history_id}/restore`)
