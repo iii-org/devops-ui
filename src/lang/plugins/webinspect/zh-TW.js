bfd40b0cfc2445a8f9927f231ed64819c4e3585d
Clay <noitseuq.rm@gmail.com>
2023-04-17 11:53:03 +0800

    feat: modify system plugin manage form title

--- src/lang/plugins/webinspect/zh-TW.js
+++ src/lang/plugins/webinspect/zh-TW.js
@@ -1,8 +1,8 @@
 export default {
   'description': 'Webinspect 原始碼掃描',
   'arguments': {
-    'wi-base-url': { title: '伺服器位址', hint: '請輸入伺服器位址', placeholder: 'Webinspect 伺服器的 URL' },
-    'wi-type': { title: 'Webinspect Type', hint: '請輸入 webinspect type', placeholder: '如果使用企業版則輸入「WIE」，否則留空。' },
+    'wi_scc_url': { title: '伺服器位址(SCC)', hint: '請輸入伺服器位址', placeholder: 'Webinspect SCC 伺服器的 URL' },
+    'wi_dast_url': { title: '伺服器位址(DAST)', hint: '請輸入伺服器位址', placeholder: 'Webinspect DAST 伺服器的 URL' },
     'wi-username': { title: '使用者名稱', hint: '請輸入使用者名稱', placeholder: 'Webinspect 的使用者名稱' },
     'wi-password': { title: '密碼', hint: '請輸入密碼', placeholder: 'Webinspect 的使用者密碼' }
   }
